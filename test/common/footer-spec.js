ad403c4535a9787f49db68fe5b480ac46705b599
Nat Burgwyn <burgwyn@gmail.com>
2016-08-04 15:18:21 -0400

    Create footer test spec

---
+++ test/common/footer-spec.js
@@ -0,0 +1,9 @@
+'use strict';
+var expect = require('chai').expect;
+var Footer = require('../../src/js/common/footer.js');
+
+describe('Footer', function() {
+    it('should exist', function() {        
+        expect(Footer).to.not.be.undefined;
+    });
+});
