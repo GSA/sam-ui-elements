7f78bbf1e51ef5df4d7780b3d099fa1374a2d350
tarandhupar <tarandeepdhupar@Tarandeeps-MBP.fios-router.home>
2016-08-04 00:12:32 -0400

    Created accordions and refactored folders
    Created accordions and refactored folders

--- src/js/misc/accordions.js
+++ src/js/misc/accordions.js
@@ -7,14 +7,18 @@ Accordions = {
         var borderPrefix = '';
         if(config.bordered)
             borderPrefix = '-bordered';
+
         
+
     	var html = '<div class="usa-accordion'+borderPrefix+'">'+
                           '<ul class="usa-unstyled-list">';
+
         var data = config.data;
         var expandedSet = false;
         for (var i in data)
         {
-            var expanded = data[i].expanded == undefined ? false : (!expandedSet && data[i].expanded);           
+            var expanded = data[i].expanded == undefined ? false : (!expandedSet && data[i].expanded);
+           
             html = html+
                    '<li>'+
                         '<button class="usa-button-unstyled" aria-expanded="'+expanded+'" aria-controls="accordions-'+(i*1+1)+'">'+
@@ -28,6 +32,7 @@ Accordions = {
             if(data[i].expanded != undefined && data[i].expanded)
                 expandedSet = true;
         }
+
                            
         html = html+'</ul></div>';
     	return html;
