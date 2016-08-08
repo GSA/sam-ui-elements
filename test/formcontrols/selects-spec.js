12fa6fcf0b1638938037e45fdee549281c08a276
Josh Bruce <josh@8foldproductivity.com>
2016-08-08 16:02:14 -0400

    Preparing radio type

--- test/formcontrols/selects-spec.js
+++ test/formcontrols/selects-spec.js
@@ -8,6 +8,35 @@ describe('#SelectExists', function() {
   });
 });
 
+describe('#SelectInitial)', function() {
+  it('should return dropdown', function() {
+
+    var expected = 
+      '<div>'+
+        '<label for="options">Dropdown label</label>'+
+        '<select name="options" id="options">'+
+          '<option value="value1">Option A</option>'+
+          '<option value="value2">Option B</option>'+
+          '<option value="value3">Option C</option>'+
+        '</select>'+
+      '</div>';
+
+    var config = {
+      type: 'dropdown',
+      label: 'Dropdown label',
+      name: 'options',
+      options: {
+        'value1': 'Option A',
+        'value2': 'Option B',
+        'value3': 'Option C'
+      }
+    };
+    var actual = Selects.select(config);
+
+    expect(actual).to.eql(expected);
+  });
+});
+
 describe('#SelectLabelIsCorrect', function() {
   it('should return correct label', function() {
 
@@ -150,7 +179,7 @@ describe('#SelectOptionsCanOnlyHaveOneSelection', function() {
   });
 });
 
-describe.only('#SelectOptionsCanBeDisabled', function() {
+describe('#SelectOptionsCanBeDisabled', function() {
   it('should be able to disable select', function() {
 
     var expected = 
@@ -180,27 +209,38 @@ describe.only('#SelectOptionsCanBeDisabled', function() {
   });
 });
 
-describe('#SelectInitial)', function() {
-  it('should return dropdown', function() {
+describe.only('#RadioInitial)', function() {
+  it('should return radio select', function() {
 
     var expected = 
       '<div>'+
-        '<label for="options">Dropdown label</label>'+
-        '<select name="options" id="options">'+
-          '<option value="value1">Option A</option>'+
-          '<option value="value2">Option B</option>'+
-          '<option value="value3">Option C</option>'+
-        '</select>'+
+        '<fieldset class="usa-fieldset-inputs">'+
+          '<legend>Historical figures 2</legend>'+
+          '<ul class="usa-unstyled-list">'+
+            '<li>'+
+              '<input id="stanton" type="radio" checked="" name="historical-figures-2" value="stanton">'+
+              '<label for="stanton">Elizabeth Cady Stanton</label>'+
+            '</li>'+
+            '<li>'+
+              '<input id="anthony" type="radio" name="historical-figures-2" value="anthony">'+
+              '<label for="anthony">Susan B. Anthony</label>'+
+            '</li>'+
+            '<li>'+
+              '<input id="tubman" type="radio" name="historical-figures-2" value="tubman">'+
+              '<label for="tubman">Harriet Tubman</label>'+
+            '</li>'+
+          '</ul>'+
+        '</fieldset>'+
       '</div>';
 
     var config = {
-      type: 'dropdown',
-      label: 'Dropdown label',
+      type: 'radio',
+      label: 'Historical figures 2',
       name: 'options',
       options: {
-        'value1': 'Option A',
-        'value2': 'Option B',
-        'value3': 'Option C'
+        'stanton': 'Elizabeth Cady Stanton',
+        'anthony': 'Susan B. Anthony',
+        'tubman': 'Harriet Tubman'
       }
     };
     var actual = Selects.select(config);
