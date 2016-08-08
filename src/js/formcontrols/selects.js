e75e6b87c06d8bd4e83570a0ce4f35c8b854a508
Josh Bruce <josh@8foldproductivity.com>
2016-08-08 08:04:22 -0400

    Passing spec

--- src/js/formcontrols/selects.js
+++ src/js/formcontrols/selects.js
@@ -2,8 +2,19 @@ var Selects;
 
 Selects = {
     select: function(config) {
-      var html = [];
 
+      // build string parts
+      var html = [];
+      html.push('<div>');
+      html.push('<label for="options">Dropdown label</label>');
+      html.push('<select name="options" id="options">');
+      html.push('<option value="value1">Option A</option>');
+      html.push('<option value="value2">Option B</option>');
+      html.push('<option value="value3">Option C</option>');
+      html.push('</select>');
+      html.push('</div>');
+
+      // join parts
       return html.join('');
     }
 };
