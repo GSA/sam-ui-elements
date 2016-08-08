83bf069055f000d1c8240e780fb7dc7c61d41d7d
Josh Bruce <josh@8foldproductivity.com>
2016-08-08 08:37:53 -0400

    Name correct

--- src/js/formcontrols/selects.js
+++ src/js/formcontrols/selects.js
@@ -10,15 +10,17 @@ Selects = {
       }
 
       // make configuration members concrete
+      // required members
       var label = config.label;
+      var name = config.name;
 
       // build string parts
       var html = [];
       html.push('<div>');
-      html.push('<label for="options">');
+      html.push('<label for="'+name+'">');
       html.push(label);
       html.push('</label>');
-      html.push('<select name="options" id="options">');
+      html.push('<select name="'+name+'" id="'+name+'">');
       html.push('<option value="value1">Option A</option>');
       html.push('<option value="value2">Option B</option>');
       html.push('<option value="value3">Option C</option>');
