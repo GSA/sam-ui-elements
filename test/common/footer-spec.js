e6afb671ed71f42c38bff4fcabee759f158b8c83
Ebunoluwa Oladipo <eshabach@gmail.com>
2016-08-11 08:44:02 -0400

    updated footer files

--- test/common/footer-spec.js
+++ test/common/footer-spec.js
@@ -14,9 +14,9 @@ describe('Footer', function() {
 						'</div><div class="iae-footer"><div class="iae-footer-body usa-grid">' +
 				         '<div class="iae-footer-logo usa-width-one-sixth">' +
 			             '<a class="image-wrap" href="http://www.gsa.gov" target="_blank"><img alt="GSA logo" src="assets/img/logo-gsa.png"></a></div>' +
-				        '<ul class="iae-footer-links usa-unstyled-list usa-width-five-sixths">' + 
+				        '<ul class="iae-footer-links usa-unstyled-list usa-width-five-sixths">' +
 					    '<li class=" usa-width-one-fourth">' +
-						'<h4 class="m_T-0 iae-footer-head">About</h4><ul class="iae-footer-links usa-unstyled-list">' + 
+						'<h4 class="m_T-0 iae-footer-head">About</h4><ul class="iae-footer-links usa-unstyled-list">' +
 						'<li class="m_B"><a href="/about/" title="What Is This Site?">What Is This Site?</a></li>' +
 						'<li class="m_B"><a href="/transition-roadmap/" title="Transition Timeline">Transition Timeline</a></li>' +
 						'<li class="m_B"><a href="https://interact.gsa.gov/group/integrated-award-environment-iae-industry-community" target="_blank" title="Join our Interact Community">Join our Interact Community</a></li>' +
