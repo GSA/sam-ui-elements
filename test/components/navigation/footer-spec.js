cc8af1f2a624b94b6ee3010d41a674f9d964c56f
Josh Bruce <josh@8foldproductivity.com>
2016-08-16 09:32:34 -0400

    Header + footer now passing

--- test/components/navigation/footer-spec.js
+++ test/components/navigation/footer-spec.js
@@ -1,6 +1,6 @@
 'use strict';
 var expect = require('chai').expect;
-var Footer = require('../../src/js/common/footer.js');
+var Footer = require('../../../src/js/common/footer.js');
 
 describe('Footer', function() {
     it('should exist', function() {        
