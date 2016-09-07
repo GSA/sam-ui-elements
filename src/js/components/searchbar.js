8d633c06f827299e42192fb4ebc3e57f7b291c1a
Diego Ruiz <diego.ruiz@reisystems.com>
2016-09-07 16:09:18 -0400

    searchbar component updates

--- src/js/components/searchbar.js
+++ src/js/components/searchbar.js
@@ -1,22 +1,36 @@
 exports.searchbar = {
   render : function(config) {    
-    var gridclass = 'usa-width-three-fourths';
-    var searchAttributes = '';
-    if(config.width){
-      gridclass = config.width;
+    var id = 'search-field-big';
+    var type = '';
+    var method = 'GET';
+    if(config.type && config.type==='big'){
+      type = ' usa-search-big';
     }
-    if(config.searchAttributes){
-      searchAttributes = config.searchAttributes;
+    else if(config.type && config.type==='small'){
+      type = ' usa-search-small';
+    }
+
+    if(config.method && config.method==='GET'){
+      method = config.method;
     }
     var html = [];
 
-    html.push('<div role="search" class="'+gridclass+'">');
-    html.push('<label class="usa-sr-only" for="search-field-big">Search big</label>');
-    html.push('<input id="search-field-big" type="search" name="keyword" placeholder="#keyword" autofocus '+config.searchAttributes+'>');
+    html.push('<form class="usa-search'+type+'" method="'+method+'">');
+    html.push('<div class="usa-width-one-fourth">');
+    html.push('<label for="index" class="usa-sr-only">Select an index</label>');
+    html.push('<select name="index">');
+    html.push('<option selected="true">Assistance Listings</option>');
+    html.push('<option>Opportunities</option>');
+    html.push('</select>');
+    html.push('</div>');
+    html.push('<div role="search" class="usa-width-three-fourths">');
+    html.push('<label class="usa-sr-only" for="'+id+'">Search</label>');
+    html.push('<input id="'+id+'" type="search" name="keyword" placeholder="#keyword" autofocus>');
     html.push('<button type="submit">');
     html.push('<span class="usa-search-submit-text">Search</span>');
     html.push('</button>');
     html.push('</div>');
+    html.push('</form>');
 
     return html.join('');
 
--- src/js/elements/form-controls/selects.js
+++ src/js/elements/form-controls/selects.js
@@ -38,7 +38,7 @@ exports.select = {
 
     var error = '';
     if (this.hasError(config)) {
-      error = ' class="usa-input-error"'
+      error = ' class="usa-input-error"';
 
     }
     html = '<div'+error+'>';
@@ -204,7 +204,7 @@ exports.select = {
         title   : config.options[optionValue],
         selected: selected,
         disabled: disabled
-      }
+      };
       var html = this.option(optionConfig);
       optionHtml.push(html);
 
@@ -289,9 +289,7 @@ exports.select = {
    * 
    */
   disabled: function(config) {
-    return (this.hasDisabled(config)) 
-      ? config.disabled 
-      : [];
+    return (this.hasDisabled(config)) ? config.disabled : [];
   },
   /**
    * @private
@@ -311,9 +309,7 @@ exports.select = {
    * 
    */
   selected: function(config) {
-    return (this.hasSelected(config)) 
-      ? config.selected 
-      : [];
+    return (this.hasSelected(config)) ? config.selected : [];
   },
   /**
    * @private
@@ -370,28 +366,28 @@ exports.select = {
    */
   isInvalidConfiguration: function(config) {
 
-    if (config.label == undefined || config.label.length < 1) {
+    if (config.label === undefined || config.label.length < 1) {
       console.log('Selects: "label" is required member. And must have value.');
       console.log(config);
       return true;
 
-    } else if (config.name == undefined || config.name.length < 1) {
+    } else if (config.name === undefined || config.name.length < 1) {
       console.log('Selects: "name" is required member. And must have value.');
       console.log(config);
       return true;
 
-    } else if (config.type == undefined || config.type.length < 1) {
+    } else if (config.type === undefined || config.type.length < 1) {
       console.log('Selects: "type" is required member. And must have value');
       console.log(config);
       return true;
 
-    } else if (config.options == undefined || Object.keys(config.options).length == 0) {
+    } else if (config.options === undefined || Object.keys(config.options).length === 0) {
       console.log('Selects: "options" is required member, and must have at least one.');
       console.log(Object.keys(config.options).length);
       console.log(config);
       return true;
 
-    } else if ((config.type == 'dropdown' || config.type == 'radio') && config.selected !== undefined && config.selected.length > 1) {
+    } else if ((config.type === 'dropdown' || config.type === 'radio') && config.selected !== undefined && config.selected.length > 1) {
       // Note: A select dropdown can only have one pre-selected option.
       //       We could solve this by:
       //       1. Only paying attention to the first one during the build.
