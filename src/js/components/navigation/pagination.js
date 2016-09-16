3984837802f5289971abf46ada5a7178f5c87e52
Carryace <sunsm1991@gmail.com>
2016-09-16 11:15:42 -0400

    Pagination code update
    Refactored getPageLinks methods in pagination, modified
    getEnumeratePageLinks, getEllipsis, getPreviousLink and getNextLink.
    Deleted totalPages and currentPage properties and started to pass
    config around.

--- src/js/components/navigation/pagination.js
+++ src/js/components/navigation/pagination.js
@@ -1,4 +1,3 @@
-
 /**
  * @Class Pagination
  *
@@ -9,11 +8,9 @@
  * @type {Object}
  *
  */
-exports.pagination={
-  range:3, // The number of pages before and after the current page
+exports.pagination = {
+  range: 3, // The number of pages before and after the current page
   threshold: 6, // The threshold to check whether ellipsis is needed
-  totalPages: 0, // Total number of pages
-  currentPage: 0, // The page that is currently selected
 
   /**
    * The enter method for developers to use which will generate the complete HTML string depending on the config object
@@ -23,25 +20,26 @@ exports.pagination={
    * offset: the number of records that will be displayed in one page
    * @returns {string}
    */
-  render: function(config){
+  render: function (config) {
 
-    if(this.isInvalidConfiguration(config)){
+    if (this.isInvalidConfiguration(config)) {
       // Return early if the configuration is invalid
       return '';
     }
 
-    this.totalPages = Math.ceil(config.totalRecords/config.offset);
-    this.currentPage = config.currentPage;
-    if(this.totalPages <= 1){
+    // If the configuration is valid, still return empty string when:
+    // 1. There are no records at all
+    // 2. The records are not enough to fill in the first page
+    if (this.getTotalPages(config) <= 1) {
       // Return empty string if there are 1 or less page
       return '';
     }
 
     var html = '';
     html += '<nav class="usa-pagination" aria-label="pagination"><ul>';
-    html += this.getPreviousLink();
-    html += this.getPageLinks();
-    html += this.getNextLink();
+    html += this.getPreviousLink(config);
+    html += this.getPageLinks(config);
+    html += this.getNextLink(config);
     html += '</ul></nav>';
 
     return html;
@@ -51,110 +49,102 @@ exports.pagination={
    * Get HTML string for previous link
    * @returns {string}
    */
-  getPreviousLink: function(){
+  getPreviousLink: function (config) {
 
     // No previous link when the current page is 1
-    if(this.currentPage === 1){
-      return '';
+    if (config.currentPage > 1) {
+      return '<li><a aria-label="previous">&lsaquo; Prev</a></li>';
     }
-    return '<li><a aria-label="previous">&lsaquo; Prev</a></li>';
+    return '';
   },
 
   /**
    * Get HTML string for next link
    * @returns {string}
    */
-  getNextLink: function(){
+  getNextLink: function (config) {
 
     // No next link when the currentPage is equal to totalPages
-    if(this.currentPage === this.totalPages){
-      return '';
+    if (config.currentPage < this.getTotalPages(config)) {
+      return '<li><a aria-label="next">Next &rsaquo;</a></li>';
     }
-    return '<li><a aria-label="next">Next &rsaquo;</a></li>';
+    return '';
   },
 
 
   /**
    * Get the HTML string for all the pages according to config
-   * @assumptions: the offset is 3, max page links can be displayed on screen is 9
+   * @assumptions: the range to show pages before and after the current page is 3, max page links can be displayed on screen is 9
    * @returns {string}
    */
-  getPageLinks: function() {
-    var pageLinksHtml = '';
-    var start, end;
-    // Total number of pages less than or equal to 10, then display all page links
-    if (this.totalPages <= 10) {
-      start = 1;
-      end = this.totalPages;
-      pageLinksHtml += this.enumeratePageLinks(start, end);
+  getPageLinks: function (config) {
+    var pageLinksHtml = [];
+    pageLinksHtml.push(this.getPageLink(1, config));//Add the first page link
+
+    var start = 2;
+    var end = this.getTotalPages(config) - 1;
+    // If total number of pages less than or equal to 10, then display all page links
+    // Otherwise, use the algorithm to calculate the start and end page to show between the first and last page
+    if(end > 9){
+      // If the current page is less than the threshold, then display first 8 page links followed by ellipsis and the last page link
+      if(config.currentPage < this.threshold){
+        end = 8;
+      }
+      // If the current page is greater than the total page minus threshold, then display the first page link followed by ellipsis and the last 8 page links
+      else if(config.currentPage > this.getTotalPages(config) - this.threshold){
+        start = end - 6;
+      }
+      // For all other conditions, display the first page link followed by ellipsis and three links before and after the current page followed by ellipsis and the last page link
+      else{
+        start = config.currentPage - this.range;
+        end = config.currentPage + this.range;
+      }
     }
-    // If the current page is less than 6, then display first 8 page links followed by ellipsis and the last page link
-    else if (this.currentPage < this.threshold) {
-      start = 1;
-      end = 8;
-      pageLinksHtml += this.enumeratePageLinks(start, end);
-      pageLinksHtml += this.getLastPageLink();
-    }
-    // If the current page is greater than the total page minus 6, then display the first page link followed by ellipsis and the last 8 page links
-    else if (this.currentPage > this.totalPages - this.threshold) {
-      start = this.totalPages - 7;
-      end = this.totalPages;
-      pageLinksHtml += this.getFirstLink();
-      pageLinksHtml += this.enumeratePageLinks(start, end);
-    }
-    // For all other conditions, display the first page link followed by ellipsis and three links before and after the current page followed by ellipsis and the last page link
-    else {
-      start = this.currentPage - this.range;
-      end = this.currentPage + this.range;
-      pageLinksHtml += this.getFirstLink();
-      pageLinksHtml += this.enumeratePageLinks(start, end);
-      pageLinksHtml += this.getLastPageLink();
-    }
-    return pageLinksHtml;
+
+    pageLinksHtml.push(this.getEnumeratePageLinks(start,end,config));//Add HTMl string between the first page and last page
+    pageLinksHtml.push(this.getPageLink(this.getTotalPages(config), config));//Add the last page link
+    return pageLinksHtml.join('');
   },
 
   /**
-   * Get unicode for ellipsis
+   * Get ellipsis wrapped as a span in a list item
    * @returns {string}
    */
-  getEllipsis: function(){
-    return '&hellip;';
+  getEllipsis: function () {
+    return '<li><span>&hellip;</span></li>';
   },
 
   /**
-   * Get HTML string for page number between start index and end index
+   * Get the total pages from configuration
+   * @returns {number}
+   */
+  getTotalPages: function (config) {
+    return Math.ceil(config.totalRecords / config.offset);
+  },
+
+  /**
+   * Get HTML string for page number between start index and end index with ellipsis added if necessary
    * @param start
    * @param end
    * @returns {string}
    */
-  enumeratePageLinks: function(start, end) {
-    var pagesHtml = '';
-    for (var i = start; i <= end; i++) {
-      pagesHtml += this.getPageLink(i);
+  getEnumeratePageLinks: function (start, end, config) {
+    var pagesHtml = [];
+
+    // Add ellipsis at the front, if the start index is larger than 2
+    if(start > 2){
+      pagesHtml.push(this.getEllipsis());
     }
-    return pagesHtml;
-  },
 
-  /**
-   * Get HTML string for the last page and ellipsis before it
-   * @returns {string}
-   */
-  getLastPageLink: function() {
-    var lastHtml = '';
-    lastHtml += '<li><span>'+this.getEllipsis()+'</span></li>';
-    lastHtml += this.getPageLink(this.totalPages);
-    return lastHtml;
-  },
+    for (var i = start; i <= end; i++) {
+      pagesHtml.push(this.getPageLink(i, config));
+    }
 
-  /**
-   * Get HTML string for the first page and ellipsis after it
-   * @returns {string}
-   */
-  getFirstLink: function() {
-    var firstHtml = '';
-    firstHtml += this.getPageLink(1);
-    firstHtml += '<li><span>'+this.getEllipsis()+'</span></li>';
-    return firstHtml;
+    // Add ellipsis at the end, if the end index is less than total page minus 1
+    if(end < this.getTotalPages(config) - 1){
+      pagesHtml.push(this.getEllipsis());
+    }
+    return pagesHtml.join('');
   },
 
   /**
@@ -163,11 +153,11 @@ exports.pagination={
    * @returns {string}: the html string for the page number
    *
    */
-  getPageLink: function(index){
+  getPageLink: function (index, config) {
     var htmlStr = '<li><a';
 
-    //Add class and aria-label if it is the current step
-    if(this.currentPage === index){
+    //Add css class and aria-label if it is the current step
+    if (config.currentPage === index) {
       htmlStr += ' class="usa-current" aria-label="current"';
     }
     htmlStr += '>' + index + '</a></li>';
@@ -179,7 +169,7 @@ exports.pagination={
    * @param config: the configuration object for pagination
    * @returns {boolean} Whether the configuration is invalid or not
    */
-  isInvalidConfiguration: function(config) {
+  isInvalidConfiguration: function (config) {
     //Check whether the config contains three variables: totalRecords, currentPage and offset
     if (config.totalRecords === undefined || config.totalRecords < 0) {
       console.log('Pagination: "totalRecords" is required member. And must have value larger than or equal to 0.');
@@ -198,7 +188,7 @@ exports.pagination={
 
     }
     // Check whether the current page is greater than the total page
-    if(config.currentPage > Math.ceil(config.totalRecords/config.offset)){
+    if (config.currentPage > this.getTotalPages(config)) {
       console.log('Pagination: "currentPage" cannot be greater than total page.');
       console.log(config);
       return true;
