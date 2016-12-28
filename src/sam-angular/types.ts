f9c4aa88116db122a132ab63489faa1e46feb8bf
Colin Alford <colinalford@gmaill.com>
2016-12-28 11:38:31 -0500

    Deleted sam-angular folder and files

--- src/sam-angular/types.ts
+++
@@ -1,6 +0,0 @@
-export type OptionsType = Array<{
-  value: string | number, // the model value
-  label: string, // the visible text for the input or option
-  name: string, // the machine readable description of the input
-  disabled?: boolean // if true, the option is greyed out and not clickable
-}>;
