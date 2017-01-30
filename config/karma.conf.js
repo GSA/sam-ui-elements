58aaec9574d6ea4f797188858a53350c954949b3
Justin Babbs <justin.babbs@octoconsulting.com>
2017-01-30 15:19:50 -0500

    use phantomjs for unit tests

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -83,7 +83,7 @@ module.exports = function(config) {
      * available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
      */
     browsers: [
-      'Chrome'
+      'PhantomJS'
     ],
 
     customLaunchers: {
