2061c2f87cdc91ceaed5673344f67d91a42b93d6
Carlos Valle <carlos.m.valle@gmail.com>
2017-04-15 17:30:30 -0400

    Theming info accordion

--- src/ui-kit/components/info-accordion/info-accordion.component.ts
+++ src/ui-kit/components/info-accordion/info-accordion.component.ts
@@ -1,8 +1,82 @@
 import { Component, Input, HostListener, Output, EventEmitter } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/core';
 
 @Component({
   selector: 'sam-info-accordion',
-  templateUrl: 'info-accordion.template.html'
+  templateUrl: 'info-accordion.template.html',
+  animations: [
+    
+    trigger('fadeInOut', [
+      state('in', style({opacity: '1'})),
+      state('out', style({opacity: '1'})),
+      transition('in => out, out => in', [
+        animate('.4s', keyframes([
+          style({ opacity: '1', transform: 'translateY(0%)', offset: 0}),
+          style({ opacity: '0', transform: 'translateY(-5%)', offset: 0.3}),
+          style({ opacity: '1', transform: 'translateY(0%)', offset: 1.0})
+        ]))
+      ])
+    ]),
+    
+    trigger('pointingInOut', [
+      state('in', style({opacity: '1', position: 'relative', top: '0'})),
+      state('out', style({opacity: '1', position: 'relative', top: '0'})),
+      transition('* => *', [
+        animate('0.03s 0.3s', keyframes([
+          style({ opacity: '0', top: '10%', offset: 0}),
+          style({ opacity: '1', top: '0%', offset: 1.0})
+        ]))
+      ])
+    ]),
+    
+    trigger('slideInOut', [
+      state('in', style({height: '*'})),
+      transition('void => *', [
+        style({
+          transform: 'translateY(-20%)',
+          overflow: 'hidden',
+          opacity: '0',
+          height: '0' 
+        }),
+        animate('.3s ease-in', style({
+          transform: 'translateY(0%)',
+          overflow: 'hidden',
+          opacity: '1',
+          height: '*'
+        }))
+      ]),
+      transition('* => void', [
+        style({
+          overflow: 'hidden',
+          opacity: '1',
+          height: '*'
+        }),
+        animate('.3s ease-out', style({
+          transform: 'translateY(-20%)',
+          overflow: 'hidden',
+          opacity: '0',
+          height: '0' 
+        }))
+      ])
+    ]),
+    
+    trigger('overState', [
+      state('inactive', style({
+        transform: 'translateY(0%) scale(1)',
+      })),
+      state('active', style({
+        transform: 'translateY(0%) scale(1.02)',
+      })),
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out')),
+      transition('void => inactive', [
+        style({
+          transform: 'translateY(10%) scale(1)',
+        }),
+        animate('.5s cubic-bezier(0.175, 0.885, 0.32, 1.275)')
+      ]),
+    ])
+  ]
 })
 export class SamInfoAccordionComponent {
 
@@ -24,11 +98,17 @@ export class SamInfoAccordionComponent {
 
   formatted: boolean = false;
   innerWidth: number = window.innerWidth;
+  
+  fadeSegmentInOut: string = '';
 
   constructor() { }
 
   ngOnInit(){
     this.formatData();
+  }
+  
+  over(item, i, j) {
+    item.state = (item.state === 'active' ? 'inactive' : 'active');
   }
 
   ngOnChanges(){
@@ -50,8 +130,7 @@ export class SamInfoAccordionComponent {
       this.formatted = true;
     }
   }
-
-
+  
   selectDetail(i, j, event){
     if(this.isCurrent(i,j) && this.detailObj.showDetail){
       this.detailObj.showDetail = false;
@@ -60,6 +139,7 @@ export class SamInfoAccordionComponent {
       this.detailObj.posX = i;
       this.detailObj.posY = j;
       this.detailObj.item = this.data[this.detailObj.posX][this.detailObj.posY];
+      this.fadeSegmentInOut = this.fadeSegmentInOut === 'in' ? 'out' : 'in';
     }
     this.closeNotification = this.name;
     this.updateNotification.emit(this.closeNotification);
@@ -94,9 +174,9 @@ export class SamInfoAccordionComponent {
 
   private getBorderClass(i, j): string{
     if(this.isCurrent(i ,j) && this.detailObj.showDetail){
-      return "item-border-select";
+      return "basic lightest blue";
     }
-    return "item-border-unselect";
+    return "inverted cool blue";
   }
 
   private largeScreen(): boolean{
@@ -111,8 +191,8 @@ export class SamInfoAccordionComponent {
 
   }
 
-  private getLinkClass(): string{
-    return this.isExternalLink? "usa-external_link":"";
+  private getLinkClass(): boolean{
+    return this.isExternalLink ? true : false;
   }
 
   private toggleDetail(i): boolean{
