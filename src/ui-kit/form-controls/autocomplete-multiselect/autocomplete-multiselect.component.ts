3136ef23c248c7be0f1af1653c3b683f7cf53c1d
Colin Alford <colinalford@gmail.com>
2017-07-06 23:59:06 -0400

    Removed console logs, lowerd timeout

--- src/ui-kit/form-controls/autocomplete-multiselect/autocomplete-multiselect.component.ts
+++ src/ui-kit/form-controls/autocomplete-multiselect/autocomplete-multiselect.component.ts
@@ -536,14 +536,10 @@ export class SamAutocompleteMultiselectComponent implements ControlValueAccessor
     if (this.service && this.options.length === 0) {
       this.cachingService.updateSearchString(searchString);
       if (this.cachingService.shouldUseCachedResults()) {
-        console.log('should be using results', this.inputTimer);
         return;
       } else {
-        console.log('current timer', this.inputTimer)
-        console.log('should clear timer')
         window.clearTimeout(this.inputTimer);
-        console.log('this should be empty', this.inputTimer);
-        this.inputTimer = window.setTimeout(this.fetchFromService, 400, searchString, options, this);
+        this.inputTimer = window.setTimeout(this.fetchFromService, 250, searchString, options, this);
         return;
           
       }
