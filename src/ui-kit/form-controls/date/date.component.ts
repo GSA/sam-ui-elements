8dec9fcbe981eceddbccfab720908188d4402ded
Isaac Kim <isaac.kim@reisystems.com>
2017-08-15 14:54:00 -0400

    Revert "Moved ‘required’ text from parent wrapper to individual child wrappers, changed labels of start and end date to from and to date"
    This reverts commit 28b5c78c988cb10e3d50f073ed492fdd50c78681.

--- src/ui-kit/form-controls/date/date.component.ts
+++ src/ui-kit/form-controls/date/date.component.ts
@@ -37,10 +37,6 @@ export class SamDateComponent implements OnInit, OnChanges, ControlValueAccessor
   */
   @Input() label: string = "";
   /**
-  * Sets the required text
-  */
-  @Input() required: string = "";
-  /**
   * Sets the helpful hint text
   */
   @Input() hint: string = "";
