8b47773206041156c87b62ad8594a9d4349c08c7
diegoaruiz <diego@reisystems.com>
2017-11-13 13:23:47 -0500

    select unit tests

--- src/ui-kit/form-controls/select/select.spec.ts
+++ src/ui-kit/form-controls/select/select.spec.ts
@@ -4,80 +4,115 @@ import { TestBed, async } from '@angular/core/testing';
 import { SamSelectComponent } from './select.component';
 import { SamWrapperModule } from '../../wrappers';
 import {SamFormService } from '../../form-service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormControl } from '@angular/forms';
+import { ChangeDetectorRef } from '@angular/core';
 
 describe('The Sam Select component', () => {
-  let component: SamSelectComponent;
-  let fixture: any;
-
-  let options = [
-    {value: 1, label: 'one', name: 'one'},
-    {value: 2, label: 'two', name: 'two'},
-    {value: 3, label: 'three', name: 'three'}
-  ];
-
-  let defaultConfig = {
-    options: options,
-    label: 'select',
-    name: 'my-select',
-  };
-
-  // provide our implementations or mocks to the dependency injector
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [SamWrapperModule,FormsModule],
-      declarations: [SamSelectComponent],
-      providers: [SamFormService]
+  describe("isolated tests", ()=>{
+    let component: SamSelectComponent;
+    let cdr: ChangeDetectorRef;
+    beforeEach(()=>{
+      component = new SamSelectComponent(cdr, new SamFormService());
     });
 
-    fixture = TestBed.createComponent(SamSelectComponent);
-    component = fixture.componentInstance;
-    component.options = defaultConfig.options;
-    component.label = defaultConfig.label;
-    component.name = defaultConfig.name;
-  });
-
-  it('should display 3 options if 3 options are specified by the config', function () {
-    fixture.detectChanges();
-    expect(fixture.nativeElement.getElementsByTagName('option').length).toBe(options.length);
-  });
-
-  it('should allow an initial value to be set by the model input', async(() => {
-    component.model = 2;
-    fixture.detectChanges();
-    setTimeout(() => {
-      let selectElement = fixture.nativeElement.getElementsByTagName('select')[0];
-      expect(selectElement.selectedIndex).toBe(1);
+    it("should implement controlvalueaccessor", ()=>{
+      component.registerOnChange((_)=>{});
+      component.registerOnTouched(()=>{});
+      component.setDisabledState(false);
+      component.writeValue(['aaa']);
+      expect(component.model[0]).toBe('aaa');
     });
-  }));
 
-  it('should show a hint message', function () {
-    let hint = "Life pro tip: eat vegetables";
-    component.hint = hint;
-    fixture.detectChanges();
-    expect(fixture.nativeElement.innerHTML).toContain(hint);
-  });
-
-  it('should show an error message', function () {
-    let errorMessage = "Uh-oh, something went wrong";
-    component.errorMessage = errorMessage;
-    fixture.detectChanges();
-    expect(fixture.nativeElement.innerHTML).toContain(errorMessage);
-  });
-
-  it('should show a label', function () {
-    let labelText = "Pick from the following options";
-    component.label = labelText;
-    fixture.detectChanges();
-    expect(fixture.nativeElement.innerHTML).toContain(labelText);
-  });
-
-  it('should disable the dropdown', async(() => {
-    component.disabled = true;
-    fixture.detectChanges();
-    setTimeout(() => {
-      let selectElement = fixture.nativeElement.getElementsByTagName('select')[0];
-      expect(selectElement.disabled).toBe(true);
+    it("should check for a name", ()=>{
+      try{
+        component.ngOnInit();
+        fail();
+      } catch (e){
+        expect(true).toBe(true);
+      }
     });
-  }));
+  });
+  describe("rendered tests", ()=>{
+    let component: SamSelectComponent;
+    let fixture: any;
+  
+    let options = [
+      {value: 1, label: 'one', name: 'one'},
+      {value: 2, label: 'two', name: 'two'},
+      {value: 3, label: 'three', name: 'three'}
+    ];
+  
+    let defaultConfig = {
+      options: options,
+      label: 'select',
+      name: 'my-select',
+    };
+  
+    // provide our implementations or mocks to the dependency injector
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [SamWrapperModule,FormsModule],
+        declarations: [SamSelectComponent],
+        providers: [SamFormService]
+      });
+  
+      fixture = TestBed.createComponent(SamSelectComponent);
+      component = fixture.componentInstance;
+      component.options = defaultConfig.options;
+      component.label = defaultConfig.label;
+      component.name = defaultConfig.name;
+    });
+  
+    it('should display 3 options if 3 options are specified by the config', function () {
+      fixture.detectChanges();
+      expect(fixture.nativeElement.getElementsByTagName('option').length).toBe(options.length);
+    });
+  
+    it('should allow an initial value to be set by the model input', async(() => {
+      component.model = 2;
+      fixture.detectChanges();
+      setTimeout(() => {
+        let selectElement = fixture.nativeElement.getElementsByTagName('select')[0];
+        expect(selectElement.selectedIndex).toBe(1);
+      });
+    }));
+  
+    it('should show a hint message', function () {
+      let hint = "Life pro tip: eat vegetables";
+      component.hint = hint;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.innerHTML).toContain(hint);
+    });
+  
+    it('should show an error message', function () {
+      let errorMessage = "Uh-oh, something went wrong";
+      component.errorMessage = errorMessage;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.innerHTML).toContain(errorMessage);
+    });
+  
+    it('should show a label', function () {
+      let labelText = "Pick from the following options";
+      component.label = labelText;
+      fixture.detectChanges();
+      expect(fixture.nativeElement.innerHTML).toContain(labelText);
+    });
+  
+    it('should disable the dropdown', async(() => {
+      component.disabled = true;
+      fixture.detectChanges();
+      setTimeout(() => {
+        let selectElement = fixture.nativeElement.getElementsByTagName('select')[0];
+        expect(selectElement.disabled).toBe(true);
+      });
+    }));
+
+    it("should work with form controls", ()=>{
+      let c = new FormControl([],()=>{ return null; });
+      component.control = c;
+      component.ngOnInit();
+      component.ngAfterViewInit();
+      
+    });
+  });
 });
