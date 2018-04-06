d327e9e54e1e8d409fa4daa007e48c04aa13c719
Colin Alford <colinalford@gmail.com>
2018-04-06 15:21:46 -0400

    Added support for icons

--- src/ui-kit/directives/external-link/external-link.directive.ts
+++ src/ui-kit/directives/external-link/external-link.directive.ts
@@ -2,7 +2,8 @@ import {
   Directive,
   AfterViewInit,
   ElementRef,
-  Renderer2
+  Renderer2,
+  Input
 } from '@angular/core';
 
 @Directive({
@@ -10,16 +11,53 @@ import {
 })
 export class SamExternalLinkDirective
   implements AfterViewInit {
+  
+  @Input() public hideIcon: boolean = false;
+
+  private get hasExternalIcon (): boolean {
+    return this.el.nativeElement
+      .querySelectorAll('.fa-external-link')
+      .length > 0;
+  }
+
+  private get renderIcon (): boolean {
+    return !(this.hasExternalIcon || this.hideIcon);
+  }
 
   constructor (
     private el: ElementRef,
     private renderer: Renderer2) {}
 
-  ngAfterViewInit () {
+  public ngAfterViewInit () {
+
+    if (this.renderIcon) {
+      this.render(this.createIcon());
+    }
+
+    this.render(this.createSrText());
+  }
+
+  private render (el): void {
+    this.renderer.appendChild(
+      this.el.nativeElement,
+      el
+    );
+  }
+
+  private createIcon (): HTMLSpanElement {
+    const icon = this.renderer.createElement('span');
+    icon.classList.add('fa', 'fa-external-link', 'fa-sm');
+    icon.setAttribute('aria-hidden', true);
+
+    return icon;
+  }
+
+  private createSrText (): HTMLSpanElement {
     const span = this.renderer.createElement('span');
     span.innerText = ' (opens in new window)';
     span.classList.add('usa-sr-only');
-    this.renderer.appendChild(this.el.nativeElement, span);
+    
+    return span;
   }
 
 }
--- src/ui-kit/directives/external-link/external-link.spec.ts
+++ src/ui-kit/directives/external-link/external-link.spec.ts
@@ -21,16 +21,34 @@ import {
 
 @Component({
   template: `
-    <a id="blank" target="_blank">Blank</a>
-    <a id="named" target="name">Named</a>
+    <a id="blank"
+      target="_blank">
+      Blank
+      <span class="fa fa-external-link fa-sm"></span>
+    </a>
+
+    <a id="named"
+      target="name">
+      Named
+    </a>
+
+    <a id="hidden"
+      target="hidden"
+      hideIcon="true">
+      Hidden
+    </a>
   `
 })
 class TestComponent {}
 
-fdescribe('Sam External Link Directive', () => {
+describe('Sam External Link Directive', () => {
   let directive: SamExternalLinkDirective;
   let component: TestComponent;
   let fixture: ComponentFixture<TestComponent>;
+
+  function findIcons (el) {
+    return el.queryAll(By.css('.fa-external-link'));
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -47,7 +65,7 @@ fdescribe('Sam External Link Directive', () => {
 
   it('should add external link text for 508 compliance',
     () => {
-    const cmps: DebugElement[] =
+    const cmps =
       fixture.debugElement.queryAll(By.css('.usa-sr-only'));
     let el1 = cmps[0];
     let el2 = cmps[1];
@@ -56,5 +74,35 @@ fdescribe('Sam External Link Directive', () => {
       .toContain('(opens in new window)');
     expect(el2.nativeElement.innerText)
       .toContain('(opens in new window)');
-  })
+  });
+
+  it('should add external link icon when none is present',
+    () => {
+    const cmp =
+      fixture.debugElement.query(By.css('#named')); 
+
+    const icons = findIcons(cmp);
+
+    expect(icons.length).toBe(icons.length);
+  });
+
+  it('should not add additional icons if one is present',
+    () => {
+    const cmp =
+      fixture.debugElement.query(By.css('#named')); 
+
+    const icons = findIcons(cmp);
+
+    expect(icons.length).toBe(1);
+  });
+
+  it('should not add icon if hideIcon is true', () => {
+    const cmp =
+      fixture.debugElement.query(By.css("#hidden"));
+    
+    const icons = findIcons(cmp);
+
+    expect(icons.length).toBe(0);
+  });
+
 });
