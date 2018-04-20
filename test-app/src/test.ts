73420504acb295439e4319df35ea6a54693e7517
diegoaruiz <diego@reisystems.com>
2018-04-20 12:01:00 -0400

    undoing changes

--- test-app/src/test.ts
+++ test-app/src/test.ts
@@ -6,7 +6,7 @@ import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
-import { TestBed } from '@angular/core/testing';
+import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
@@ -20,39 +20,14 @@ declare const require: any;
 __karma__.loaded = function () {};
 
 // First, initialize the Angular testing environment.
-const testBed = TestBed.initTestEnvironment(
+getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
-patchTestBedToDestroyFixturesAfterEveryTest(testBed);
+
 // Then we find all the tests.
 const context = require.context('../../src/', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
 // Finally, start Karma to run the tests.
 __karma__.start();
-
-
-function patchTestBedToDestroyFixturesAfterEveryTest(testBed) {
-  // Original resetTestingModule function of the TestBed.
-  const _resetTestingModule = testBed.resetTestingModule;
-
-  // Monkey-patch the resetTestingModule to destroy fixtures outside of a try/catch block.
-  // With https://github.com/angular/angular/commit/2c5a67134198a090a24f6671dcdb7b102fea6eba
-  // errors when destroying components are no longer causing Jasmine to fail.
-  testBed.resetTestingModule = function() {
-    try {
-      this._activeFixtures.forEach(fixture => fixture.destroy());
-    } finally {
-      this._activeFixtures = [];
-      // Regardless of errors or not, run the original reset testing module function.
-      _resetTestingModule.call(this);
-    }
-  };
-
-  // Angular's testing package resets the testing module before each test. This doesn't work well
-  // for us because it doesn't allow developers to see what test actually failed.
-  // Fixing this by resetting the testing module after each test.
-  // https://github.com/angular/angular/blob/master/packages/core/testing/src/before_each.ts#L25
-  afterEach(() => testBed.resetTestingModule());
-}
