7c947db4d3678db5f62efcde8640cf87358f886f
solomonkabetemere <solomon.kabetemere@gsa.gov>
2018-05-09 16:10:24 -0400

    video player component

--- src/ui-kit/experimental/video-player/video-player.component.ts
+++ src/ui-kit/experimental/video-player/video-player.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit, ContentChildren, QueryList, ElementRef, forwardRef} from '@angular/core';
 import GLOBAL_STRINGS from 'accessible-html5-video-player/js/strings.js'
-import InitPxVideo from 'accessible-html5-video-player/js/px-video.js';
+import * as InitPxVideo from 'accessible-html5-video-player/js/px-video.js';
 declare var InitPxVideo: any;
+
+// NOTE: add px-video.js and strings.js files to your application .angular-cli.json script tag
 
 interface InitPxVideoConfig {
   "videoId": string,
@@ -16,28 +18,41 @@ interface InitPxVideoConfig {
   templateUrl: './video-player.template.html'
 })
 
-export class SamVideoPlayerComponent implements OnInit {
+export class SamVideoPlayerComponent {
+  @ContentChildren('videoPly', {descendants: true}) public videos: QueryList<ElementRef>;
+  @ContentChildren('videoTrack', {descendants: true}) public tracks: QueryList<ElementRef>;
+  @ContentChildren('videoSrc', {descendants: true}) public sources: QueryList<ElementRef>;
 
-  @Input() width: string;
-  @Input() height: string;
-  @Input() poster: string;
-  @Input() srcUrl: string;
-  @Input() public config: InitPxVideoConfig;
-  
-  constructor(){}
+  @Input() public videoId: string;
+  @Input() public title: string;
+  @Input() public captionOption: boolean;
+  @Input() public seekInterval: number;
+  private config: InitPxVideoConfig;
 
-  ngOnInit(){
-    
-  }
   ngAfterContentInit() {
-    console.log('test==='+InitPxVideo)
-    // Initialize
-    new InitPxVideo({
-      "videoId": "myvid",
-      "captionsOnDefault": true,
-      "seekInterval": 20,
-      "videoTitle": "clips",
+    if (this.videos.length === 0) {
+      console.error('SamVideoComponent must be provide a <video> element to function or provide template variable #videoPly');
+    }
+
+    if (this.sources.length === 0) {
+      console.error('SamVideoComponent must be provide a <source> element to functionor or provide template variable #videoTrack');
+    }
+  
+    if (this.tracks.length === 0) {
+      console.error('SamVideoComponent must be provide a <track> element with captions for 508 complianceor or provide template variable #videoSrc');
+    }
+  }
+
+  ngAfterViewInit() {
+    this.config = {
+      "videoId": this.videoId,
+      "captionsOnDefault": this.captionOption ? this.captionOption : true,
+      "seekInterval": this.seekInterval ? this.seekInterval : 10,
+      "videoTitle": this.title ? this.title : 'Sam Video',
       "debug": true
-    });
+    }
+
+    // Initialize video player
+    new InitPxVideo(this.config);
   }
 }
