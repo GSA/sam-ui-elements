f795f91bcbd660d765eb425d1f1ac86aac8ab8db
diegoaruiz <diego@reisystems.com>
2018-06-01 11:44:38 -0400

    tweaks to filter drawere

--- src/ui-kit/experimental/filter-drawer/filter-drawer.component.ts
+++ src/ui-kit/experimental/filter-drawer/filter-drawer.component.ts
@@ -1,19 +1,26 @@
-import { Component, ContentChildren, Directive, Input, QueryList} from '@angular/core';
+import { Component, Output, EventEmitter, HostListener, ContentChildren, Directive, Input, QueryList} from '@angular/core';
+
 
 @Component({
 
-  selector: "sam-filter-drawer-item",
+  selector: "[sam-filter-drawer-item]",
   template: `
-    <li>
-      <span>{{this.fieldLabel}}:</span> {{this.fieldValue}}
-    </li>
+    <span>{{this.fieldLabel}}:</span> {{this.fieldValue}} <i class="icon close fa fa-times"></i>
   `
  })
 
 export class SamFilterDrawerItemComponent {
+  @HostListener('click') itemClick(){
+    this.remove.emit({
+      id: this.fieldId,
+      label: this.fieldLabel,
+      value: this.fieldValue
+    });
+  }
   @Input() public fieldId: string;
   @Input() public fieldLabel: string;
   @Input() public fieldValue: string;
+  @Output() public remove: EventEmitter<any> = new EventEmitter();
 }
 
 @Component({
