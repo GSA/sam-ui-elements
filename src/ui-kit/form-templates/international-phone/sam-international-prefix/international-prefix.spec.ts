0e3e62b5d656c7fb444c99a8b8bc0701b2e40e82
Colin Alford <colinalford@gmail.com>
2018-06-28 16:27:08 -0400

    Fixed test

--- src/ui-kit/form-templates/international-phone/sam-international-prefix/international-prefix.spec.ts
+++ src/ui-kit/form-templates/international-phone/sam-international-prefix/international-prefix.spec.ts
@@ -1,49 +1,102 @@
+import { ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormsModule,
+  FormControl,
+  ReactiveFormsModule } from '@angular/forms'; 
+import { TestBed } from '@angular/core/testing';
+
 import {
   SamInternationalPrefix
 } from './international-prefix.component';
-import { FormControl } from '@angular/forms';
+import { SamFormService } from '../../../form-service';
+import { SamWrapperModule } from '../../../wrappers'; 
+import { SamFormControlsModule } from '../../../form-controls';
+
 
 describe('Sam International Prefix', () => {
 
   let component: SamInternationalPrefix;
 
-  beforeEach(() => {
-    component = new SamInternationalPrefix(null, null);
-    component.name = 'tel';
-    component.label = 'Phone';
-  });
+  describe('Standalone tests', () => {
 
-  it('should set internal value when input changes', () => {
-    const mock = {
-      currentTarget: {
-        value: 5
-      }
-    };
+    beforeEach(() => {
+      component = new SamInternationalPrefix(null, null);
+      component.name = 'tel';
+      component.label = 'Phone';
+    });
+  
+    it('should validate country code less than 999', () => {
+      const value = 5;
+      const control = new FormControl();
+      control.setValue(value);
+  
+      const errs = component.validate(control);
+  
+      expect(errs).toBe(null);
+    });
+  
+    it('should invalidate country code > 999', () => {
+      const value = 1000;
+      const errMsg = 'Country codes must be 3 numbers or fewer';
+      const control = new FormControl();
+      control.setValue(value);
+  
+      const errs = component.validate(control);
+  
+      expect(errs.countryCode.message).toBe(errMsg);
+    });
+  })
 
-    component.inputChange(mock);
 
-    expect(component.value).toBe(5);
-  });
 
-  it('should validate country code less than 999', () => {
-    const value = 5;
-    const control = new FormControl();
-    control.setValue(value);
+  describe('Rendered tests', () => {
+    let fixture;
 
-    const errs = component.validate(control);
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [
+          CommonModule,
+          FormsModule,
+          FormsModule,
+          ReactiveFormsModule,
+          SamWrapperModule,
+          SamFormControlsModule,
+        ],
+        declarations: [
+          SamInternationalPrefix,
+        ],
+        providers: [
+          SamFormService,
+          ChangeDetectorRef
+        ]
+      });
+  
+      fixture = TestBed.createComponent(SamInternationalPrefix);
+      component = fixture.componentInstance;
+      component.name = 'tel';
+      component.label = 'Phone';
+    });
 
-    expect(errs).toBe(null);
-  });
+    it('should set internal value when input changes', () => {
+      const mock = {
+        currentTarget: {
+          value: '5'
+        }
+      };
+  
+      component.inputChange(mock);
 
-  it('should invalidate country code > 999', () => {
-    const value = 1000;
-    const errMsg = 'Country codes must be 3 numbers or fewer';
-    const control = new FormControl();
-    control.setValue(value);
+      fixture.detectChanges();
+      
+      fixture.whenStable().then(
+        () => {
+          const val = fixture.debugElement.querySelector('.sam-international-prefix').value;
+          expect(val).toBe(5);
+        }
+      )
 
-    const errs = component.validate(control);
 
-    expect(errs.countryCode.message).toBe(errMsg);
-  });
-
+    });
+  })
 });
