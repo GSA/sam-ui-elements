11655395bb5d6be6a68dd64684f965191a520682
Carlos Valle <carlos.m.valle@gmail.com>
2018-07-18 14:52:28 -0400

    Close sidenav for small screen devices

--- src/ui-kit/experimental/patterns/layout/components/page/page.component.ts
+++ src/ui-kit/experimental/patterns/layout/components/page/page.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
   ElementRef,
+  HostListener,
   ViewEncapsulation,
   Renderer2,
   NgZone,
@@ -17,41 +18,54 @@ import { SamPageNextService } from '../../architecture';
   encapsulation: ViewEncapsulation.None,
 })
 export class SamPageNextComponent
-  extends MdSidenavContainer {
-
+extends MdSidenavContainer {
+  
+  @HostListener('window:resize')
+  resize() { if (this.aside) { this._responsiveAside(); } }
+  
   @ContentChild(MdSidenav) public aside: MdSidenav;
-
+  
   @ContentChild(SamToolbarComponent)
-    public toolbar: SamToolbarComponent;
-
+  public toolbar: SamToolbarComponent;
+  
   constructor(
     _element: ElementRef,
     _renderer: Renderer2,
     _ngZone: NgZone) {
-    super(null, _element, _renderer, _ngZone);
-  }
-
-  ngAfterContentInit () {
-    super.ngAfterContentInit();
-
-    this._setupAside();
-    this._setupToolbar();
-  }
-
-  private _setupAside () {
-    if (this.aside) {
-      this.aside.opened = true;
-      this.aside.mode = 'side';
+      super(null, _element, _renderer, _ngZone);
     }
-  }
-
-  private _setupToolbar () {
-    if (this.toolbar) {
+    
+    ngAfterContentInit () {
+      super.ngAfterContentInit();
+      
+      this._setupAside();
+      this._setupToolbar();
+    }
+    
+    private _setupAside () {
       if (this.aside) {
-        // Attach sidenav to toolbar
-        this.toolbar.sidenav = this.aside;
+        this._responsiveAside();
       }
     }
-  }
+    
+    private _setupToolbar () {
+      if (this.toolbar) {
+        if (this.aside) {
+          // Attach sidenav to toolbar
+          this.toolbar.sidenav = this.aside;
+        }
+      }
+    }
 
-}
+    
+    private _responsiveAside () {
+      this.aside.mode = window.innerWidth > 600 ? 'side' : 'over';
+      if(this.aside.opened && window.innerWidth < 600){
+        this.aside.opened = false;
+      }else if(!this.aside.opened && window.innerWidth > 600){
+        this.aside.opened = true;
+      }
+    }
+    
+  }
+  
