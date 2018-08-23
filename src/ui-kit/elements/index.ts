a834b6040ddf360e6179b654f5b0a46496d19b37
Colin Alford <colinalford@gmail.com>
2018-08-23 12:06:30 -0400

    Modularized code

--- src/ui-kit/elements/index.ts
+++ src/ui-kit/elements/index.ts
@@ -1,19 +1,2 @@
-import { NgModule} from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { SamButtonComponent } from './button';
-
-@NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [
-    SamButtonComponent
-  ],
-  exports: [
-    SamButtonComponent
-  ]
-})
-export class SamElementsModule { }
-
 export * from './button';
+export * from './elements.module';
--- src/ui-kit/wrappers/index.ts
+++ src/ui-kit/wrappers/index.ts
@@ -1,15 +1,3 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { FieldsetWrapper } from './fieldset-wrapper';
-import { LabelWrapper } from './label-wrapper';
-
-@NgModule({
-  imports: [ CommonModule ],
-  declarations: [ FieldsetWrapper, LabelWrapper ],
-  exports: [ FieldsetWrapper, LabelWrapper ]
-})
-export class SamWrapperModule {}
-
 export * from './fieldset-wrapper';
 export * from './label-wrapper';
+export * from './wrappers.module';
