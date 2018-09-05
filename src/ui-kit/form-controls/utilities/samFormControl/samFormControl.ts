0de3d9784b7db45a65776d2a50e2f33101dc607b
Colin Alford <colinalford@gmail.com>
2018-09-05 15:59:08 -0400

    Set HTML  input value when samModel receives new value

--- src/ui-kit/form-controls/utilities/samFormControl/samFormControl.ts
+++ src/ui-kit/form-controls/utilities/samFormControl/samFormControl.ts
@@ -4,21 +4,35 @@ import {
   ElementRef,
   Renderer2,
   EventEmitter,
-  Output
+  Output,
+  OnChanges
 } from '@angular/core';
 
 @Directive({
   selector: '[samFormControl]'
 })
-export class SamFormControlDirective {
+export class SamFormControlDirective implements OnChanges {
   @Input() public emitOn = 'input';
   @Input() public samModel: any;
   @Output() public samModelChange = new EventEmitter<any>();
 
-  constructor(private _el: ElementRef, private _renderer: Renderer2) { }
+  constructor(private _el: ElementRef,
+    private _renderer: Renderer2) { }
   
+  public ngOnChanges () {
+      this._renderer.setAttribute(
+        this._el.nativeElement,
+        'value',
+        this.samModel
+      );
+  }
+
   public ngOnInit () {
-    this._renderer.listen(this._el.nativeElement, this.emitOn, this._changeEvent.bind(this));
+    this._renderer.listen(
+      this._el.nativeElement,
+      this.emitOn,
+      this._changeEvent.bind(this)
+    );
   }
 
   private _changeEvent (event: any) {
