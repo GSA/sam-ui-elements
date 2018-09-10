532c74529b035cd19920a0be95a414be36af0ecc
colinalford-gsa <31138862+colinalford-gsa@users.noreply.github.com>
2018-09-10 10:41:51 -0400

    Revert "[Review] Updating button API"

--- src/ui-kit/elements/button/button.component.ts
+++ src/ui-kit/elements/button/button.component.ts
@@ -1,76 +1,47 @@
 import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 /**
-* The <sam-button> component generates a button for user interaction
-*/
+ * The <sam-button> component generates a button for user interaction
+ */
 @Component({
   selector: 'sam-button',
   template: `
-  <button
-    [attr.id]="id"
-    class="sam button" 
-    [ngClass]="btnClass" 
-    [attr.disabled]="isDisabled ? '' : null" 
-    (click)="click($event)" 
-    [type]="action=='submit' ? 'submit' : 'button'">
-    {{ buttonText }}
-    <ng-content></ng-content>
-  </button>`,
+    <button
+      id={{buttonId}} 
+      class="sam-ui button" 
+      [ngClass]="btnClass" 
+      [attr.disabled]="buttonDisabled ? '' : null" 
+      (click)="click($event)" 
+      [type]="buttonType=='submit' ? 'submit' : 'button'">
+      {{buttonText}}
+    </button>`,
 })
 export class SamButtonComponent {
   /**
   * Sets the id that will assign to the button element
   */
-  private _id = '';
-  @Input()
-  set id(id: string) {
-    this._id = id;
-  }
-  get id(): string { 
-    return this._id || this.buttonId; 
-  }
+  @Input() buttonId: string;
   
   /**
-  * Sets the action of the button
-  * (default,primary,secondary,tertiary,submit)
+  * Sets the text content that will show on the button
   */
-  private _action: string;
-  @Input() 
-  set action(action: string){
-    this._action = action;
-  };
-  get action(): string {
-    return this._action || this.buttonType;
-  }
+  @Input() buttonText: string;
+
+  /**
+  * Sets the type of the button
+  * (default,alt,secondary,outline,gray,disabled,big)
+  */
+  @Input() buttonType: string;
   
   /**
   * Sets the button size
   */
-  private _size: string;
-  @Input() 
-  set size(size: string){
-    this._size = size;
-  };
-  get size():string {
-    return this._size || this.buttonSize;
-  }
+  @Input() buttonSize: string;
   
   /**
   * Disables the button
   */
-  private _isDisabled: boolean = false;
-  @Input() 
-  set isDisabled(isDisabled: boolean){
-    this._isDisabled = isDisabled;
-  };
-  get isDisabled():boolean {
-    return this._isDisabled || this.buttonDisabled;
-  }
-  
-  /**
-  * Available options: dark
-  */
-  @Input() theme: string;
+  @Input() buttonDisabled: boolean = false;
   
   /**
   * Emmits event on click
@@ -78,124 +49,45 @@ export class SamButtonComponent {
   @Output() onClick: EventEmitter<any> = new EventEmitter();
   
   
-  
-  /**
-  * Sets the id that will assign to the button element (Deprecated)
-  */
-  @Input() buttonId: string;
-  
-  /**
-  * Sets the text content that will show on the button (Deprecated)
-  */
-  @Input() buttonText: string;
-  
-  /**
-  * Sets the type of the button (Deprecated)
-  */
-  @Input() buttonType: string;
-  
-  /**
-  * Sets the button size (Deprecated)
-  */
-  @Input() buttonSize: string;
-  
-  /**
-  * Disables the button (Deprecated)
-  */
-  @Input() buttonDisabled: boolean = false;
-  
   /**
   * Sets the button css class (Deprecated)
   */
   @Input() buttonClass: string = '';
   
-  
+
   private btnClassMap: any = {
     // Types
-    default: 'secondary',
+    default: 'primary',
     primary: 'primary',
     secondary: 'secondary',
-    tertiary: 'tertiary',
-    negative: 'secondary', // (Deprecated)
+    tertiary: 'basic blue',
+    negative: 'negative',
     submit: 'primary',
     // Sizes
-    small: 'small',
-    large: '', // (Deprecated)
-    // Theme
-    dark: 'inverted'
+    small: 'tiny',
+    large: 'large',
   };
-  
+
   get btnClass(): String {
     const classMap = [];
     
-    // Adds action class
-    if (this.btnClassMap.hasOwnProperty(this.action)) {
-      classMap.push(this.btnClassMap[this.action]);
+    // Adds type css class
+    if (this.btnClassMap.hasOwnProperty(this.buttonType)) {
+      classMap.push(this.btnClassMap[this.buttonType]);
     } else {
       classMap.push(this.btnClassMap.default);
     }
     
-    // Add size class
-    if (this.btnClassMap.hasOwnProperty(this.size)) {
-      classMap.push(this.btnClassMap[this.size]);
+    // Add size css class
+    if (this.btnClassMap.hasOwnProperty(this.buttonSize)) {
+      classMap.push(this.btnClassMap[this.buttonSize]);
     }
-    
-    // Adds theme class
-    if (this.btnClassMap.hasOwnProperty(this.theme)) {
-      classMap.push(this.btnClassMap[this.theme]);
-    }
-    
-    // Adds disabled class
-    if (this.isDisabled) {
-      classMap.push('disabled');
-    }
-    
+
     return classMap.join(' ');
   }
-  
-  debug(){
-    function Deprecated(deprecated, use) {
-      this.Deprecated = deprecated;
-      this.Use = use;
-      this.UpdateBy = "PI 16 Sprint 2"
-    }
-    
-    let deprecated = [];
-    
-    if(this.buttonId){
-      deprecated.push(new Deprecated("buttonId", "id"));
-    }
-    if(this.buttonText){
-      deprecated.push(new Deprecated("buttonText", "<sam-button>Your Text</sam-button>"));
-    }
-    if(this.buttonType){
-      deprecated.push( new Deprecated("buttonType", "action"));
-      if(this.buttonType == "negative"){
-        deprecated.push( new Deprecated("buttonType='negative'", "action='secondary'"));
-      }
-    }
-    if(this.buttonSize){
-      deprecated.push(new Deprecated("buttonSize", "size"));
-      if(this.buttonType == "large"){
-        deprecated.push( new Deprecated("buttonSize='large'", "Large size is not available - remove buttonSize to use normal size"));
-      }
-    }
-    if(this.buttonDisabled){
-      deprecated.push(new Deprecated("buttonDisabled", "isDisabled"));
-    }
-    if(this.buttonClass){
-      deprecated.push(new Deprecated("buttonClass", "Please remove this input"));
-    }
-    
-    if(deprecated.length > 0){
-      console.info("Button its using deprecated inputs, please update inputs");
-    }
-    
-    console.table(deprecated);
-  }
-  
+
   click($event) {
-    if (!this.isDisabled) {
+    if (!this.buttonDisabled) {
       this.onClick.emit($event);
     }
   }
