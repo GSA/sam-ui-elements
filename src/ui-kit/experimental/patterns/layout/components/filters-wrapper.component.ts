48a59b1083b937d9ba1ab87663722b82b5a50845
Colin Alford <colinalford@gmail.com>
2018-09-10 14:14:18 -0400

    Moved filters action to only update on button click

--- src/ui-kit/experimental/patterns/layout/components/filters-wrapper.component.ts
+++ src/ui-kit/experimental/patterns/layout/components/filters-wrapper.component.ts
@@ -2,25 +2,57 @@ import {
   Component,
   Input,
   OnInit,
+  OnDestroy,
   Optional
 } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+
+import { Subject } from 'rxjs/Subject';
+import { Subscription } from 'rxjs/Subscription';
 
 import { SamPageNextService } from '../architecture';
 
 
 @Component({
   selector: 'sam-filters-wrapper',
-  template: '<ng-content></ng-content>'
+  template: `
+    <ng-content></ng-content>
+    <sam-button-next
+      action="primary"
+      (onClick)="runReportEvent.next()">
+      Run Report
+    </sam-button-next>
+  `
 })
 export class SamFiltersWrapperComponent
-  implements OnInit {
+  implements OnInit, OnDestroy {
+
   @Input() public group: FormGroup;
+
+  public runReportEvent = new Subject<any>();
+  private _runReportSubscription: Subscription;
+  private _filtersSubscription: Subscription;
 
   constructor (@Optional() private _service: SamPageNextService) {}
 
   public ngOnInit () {
     this._initializeService();
+    this._initializeHandlers();
+  }
+
+  public ngOnDestroy () {
+    this._runReportSubscription.unsubscribe();
+    this._filtersSubscription.unsubscribe();
+  }
+
+  private _initializeHandlers () {
+    this._runReportSubscription = 
+      this.runReportEvent.subscribe(
+        (_) => {
+          this._service.model.properties['filters']
+            .setValue(this.group.value);
+        }
+      )
   }
 
   private _initializeService () {
@@ -31,23 +63,21 @@ export class SamFiltersWrapperComponent
         this.group.value
       );
 
-      // Update layout model when values change
-      this.group.valueChanges.subscribe(
-        changes => this._service.model
-          .properties['filters'].setValue(changes)
-      );
-
       // Listen for external changes to value and update form
-      this._service.model.properties['filters']
-        .valueChanges.subscribe(
-          event => {
-            try {
-              this.group.setValue(event, { emitEvent: false })
-            } catch (e) {
-              return;
+      this._filtersSubscription = 
+        this._service.model.properties['filters']
+          .valueChanges.subscribe(
+            event => {
+              try {
+                this.group.setValue(
+                  event,
+                  { emitEvent: false }
+                );
+              } catch (e) {
+                return;
+              }
             }
-          }
-        );
+          );
     }
   }
 }
