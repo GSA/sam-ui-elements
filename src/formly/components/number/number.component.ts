6629238662c87a09ec3fc14c60103e86bbdd60f7
hieutrang-reisys <hieu.trang@reisystems.com>
2018-09-18 14:49:40 -0400

    Formly Wrappers

---
+++ src/formly/components/number/number.component.ts
@@ -0,0 +1,27 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ViewChild,
+  ChangeDetectorRef
+} from '@angular/core';
+
+import {AbstractSamFormly} from '../../sam-formly';
+import {SamNumberComponent} from '../../../ui-kit';
+
+@Component({
+  template: `
+   <sam-number [formControl]="formControl"
+    [control]="formControl"
+   ></sam-number>
+ `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SamFormlyNumber extends AbstractSamFormly {
+  @ViewChild(SamNumberComponent)
+  public template: SamNumberComponent;
+
+  constructor(_cdr: ChangeDetectorRef) {
+    super();
+    this.cdr = _cdr;
+  }
+}
