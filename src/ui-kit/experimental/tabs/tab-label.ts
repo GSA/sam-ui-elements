18b9bdd4e2ce5032e529f0d4ca6d082d3c6a796a
Carlos Valle <carlos.m.valle@gmail.com>
2018-10-05 02:10:02 -0400

    UI updates to sam-tab-next

---
+++ src/ui-kit/experimental/tabs/tab-label.ts
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {Directive, TemplateRef, ViewContainerRef} from '@angular/core';
+import {TemplatePortalDirective} from '@angular/cdk';
+
+/** Workaround for https://github.com/angular/angular/issues/17849 */
+export const _MdTabLabelBaseClass = TemplatePortalDirective;
+
+/** Used to flag tab labels for use with the portal directive */
+@Directive({
+  selector: '[sam-tab-label]',
+})
+export class MdTabLabel extends _MdTabLabelBaseClass {
+  constructor(templateRef: TemplateRef<any>, viewContainerRef: ViewContainerRef) {
+    super(templateRef, viewContainerRef);
+  }
+}
