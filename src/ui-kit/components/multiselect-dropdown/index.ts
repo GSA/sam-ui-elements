ae511765a3504af7b365875e9643c1c5e30cc765
murali101002 <murali.akurathi@gmail.com>
2018-10-17 16:44:56 -0400

    change absolute to relative path

--- src/ui-kit/components/multiselect-dropdown/index.ts
+++ src/ui-kit/components/multiselect-dropdown/index.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SamMultiSelectDropdownComponent } from './multiselect-dropdown.component';
 import { SamCheckboxModule } from '../../form-controls/checkbox'; 
-import { SamClickOutsideModule } from 'ui-kit/directives';
+import { SamClickOutsideModule } from '../../../ui-kit/directives';
 @NgModule({
     declarations: [ SamMultiSelectDropdownComponent ],
     exports: [ SamMultiSelectDropdownComponent ],
