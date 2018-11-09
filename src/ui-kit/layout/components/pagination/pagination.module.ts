1bdbdaad9c169fae0ae62345127ba16c5a78f1db
Thomas Tighe <ttighe@reisystems.com>
2018-11-09 13:17:33 -0500

    adding import for comp

--- src/ui-kit/layout/components/pagination/pagination.module.ts
+++ src/ui-kit/layout/components/pagination/pagination.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SamPaginationNextComponent } from './pagination.component';
 import { FormsModule } from '@angular/forms';
-
+import { SamIconsModule } from '../../../../ui-kit/experimental/icon/icon.module';
 
 @NgModule({
     imports: [
-        CommonModule,FormsModule
+        CommonModule,FormsModule,
+        SamIconsModule
     ],
     declarations: [
         SamPaginationNextComponent
