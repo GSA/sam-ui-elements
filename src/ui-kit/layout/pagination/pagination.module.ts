9be8bb344e7cc1ed65134f160e796e37fe899f7b
Thomas Tighe <ttighe@reisystems.com>
2018-11-12 15:07:59 -0500

    resolving issue from directory change

--- src/ui-kit/layout/pagination/pagination.module.ts
+++ src/ui-kit/layout/pagination/pagination.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SamPaginationNextComponent } from './pagination.component';
 import { FormsModule } from '@angular/forms';
-import { SamIconsModule } from '../../../../ui-kit/experimental/icon/icon.module';
+import { SamIconsModule } from '../../../ui-kit/experimental/icon/icon.module';
 
 @NgModule({
     imports: [
