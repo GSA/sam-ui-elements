fff5792d4e0d01e311985d52fefeba6bc050c83c
Thomas Tighe <ttighe@reisystems.com>
2018-11-28 14:15:48 -0500

    Adding input and label for autocomplete

--- src/ui-kit/experimental/hierarchical/autocomplete/autocomplete.component.ts
+++ src/ui-kit/experimental/hierarchical/autocomplete/autocomplete.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SamHiercarchicalServiceInterface } from '../hierarchical-interface';
 
 @Component({
   selector: 'sam-hierarchical-autocomplete',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SamHierarchicalAutocompleteComponent implements OnInit {
 
+
+  @Input()
+  public service: SamHiercarchicalServiceInterface;
+
+  public inputValue: string;
+
   constructor() { }
 
   ngOnInit() {
+
   }
+
+  public clearInput() { }
+
+  public onChange() {
+
+  }
+
+  public inputFocusHandler() { }
+  public onKeyup() { }
+
+}
+
+
+export class SamHierarchicalAutocompleteSettings {
+
+  public labelText: string;
+  public labelhint: string;
+  public labelShowFullHint: boolean;
+  public id: string;
+  public required: boolean;
+  public erorMessage: string;
 
 }
