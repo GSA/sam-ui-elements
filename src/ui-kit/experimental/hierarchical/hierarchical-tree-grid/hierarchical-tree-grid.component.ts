c2d99d47f76646734f40cdb94e607166af060640
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2018-12-05 21:53:54 -0500

    data table works without example data source

--- src/ui-kit/experimental/hierarchical/hierarchical-tree-grid/hierarchical-tree-grid.component.ts
+++ src/ui-kit/experimental/hierarchical/hierarchical-tree-grid/hierarchical-tree-grid.component.ts
@@ -1,10 +1,14 @@
 import {
   Component,
-  OnInit, ViewChild, ElementRef,Input
+  OnInit, ViewChild, ElementRef, Input
 } from '@angular/core';
-import { SamSortDirective, SamPaginationComponent } from '../../../components'
+import { SamSortDirective, SamPaginationComponent, SamSortable } from '../../../components'
 import { ExampleDatabase, ExampleDataSource } from '../data-source';
 import { SamHiercarchicalServiceInterface } from '../hierarchical-interface';
+
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs';
+import { DataSource } from '@angular/cdk';
 
 @Component({
   selector: 'sam-hierarchical-tree-grid',
@@ -15,24 +19,99 @@ import { SamHiercarchicalServiceInterface } from '../hierarchical-interface';
 export class SamHierarchicalTreeGridComponent implements OnInit {
   @Input()
   public service: SamHiercarchicalServiceInterface;
+  @Input() public templateConfigurations: any;
+  displayedColumns = ['cfdaNumber', 'agency', 'title'];
 
-  displayedColumns = ['cfdaNumber','agency',  'title'];
   exampleDatabase = new ExampleDatabase();
-  dataSource: ExampleDataSource | null;
+  dataSource: SampleDataSource | null;
   @ViewChild(SamPaginationComponent) paginator: SamPaginationComponent;
   @ViewChild(SamSortDirective) sort: SamSortDirective;
   @ViewChild('filter') filter: ElementRef;
-
+  data:any[];
   ngOnInit() {
     this.service.getDataByText(null).subscribe(
-      (data) => {
-        console.log("Null get data by text");
-        console.log(data);
+      (res) => {
+        this.data = res;
+        // console.log("Null get data by text");
+        // console.log(res);
       });
-    this.dataSource = new ExampleDataSource(
+    this.dataSource = new SampleDataSource(
       this.exampleDatabase,
       this.paginator,
       this.sort
     );
+  }
+}
+
+
+
+export class SampleDatabase {
+  dataChange: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
+  get data(): any[] { return this.dataChange.value; }
+  constructor() {
+    for (let i = 0; i < 1940; i++) { this.addProgram(); }
+  }
+  addProgram() {
+    const copiedData = this.data.slice();
+    copiedData.push(this.createNewProgram());
+    this.dataChange.next(copiedData);
+  }
+  private createNewProgram() {
+   // return ReportData[this.data.length];
+  }
+}
+
+export class SampleDataSource extends DataSource<any> {
+  totalcost = 0;
+  _filterChange = new BehaviorSubject('');
+  get filter(): string { return this._filterChange.value; }
+  set filter(filter: string) { this._filterChange.next(filter); }
+  //filteredData: any[] = [];
+  renderedData: any[] = [];
+
+  constructor(private _exampleDatabase: any,
+    private _paginator: SamPaginationComponent,
+    private _sort: SamSortDirective) {
+    super();
+  }
+
+  connect(): Observable<any[]> {
+    const displayDataChanges = [
+      this._exampleDatabase.dataChange,
+      this._sort.samSortChange,
+      this._filterChange,
+    ];
+    return Observable.merge(...displayDataChanges).map(() => {
+
+      const filteredData = this._exampleDatabase.data.slice().filter((item: any) => {
+        const searchStr = (item.agency + item.title).toLowerCase();
+        return searchStr.indexOf(this.filter.toLowerCase()) !== -1;
+      });
+      // set total
+      this.totalcost = 0;
+      filteredData.map((item: any) => {
+        this.totalcost += item.cost;
+      });
+      // Sort filtered data
+      const sortedData = this.sortData(filteredData.slice());
+      this.renderedData = sortedData;
+      return this.renderedData;
+    });
+  }
+  disconnect() { }
+  /** Returns a sorted copy of the database data. */
+  sortData(data: any[]): any[] {
+    if (!this._sort.active) { return data; }
+    return data.sort((a, b) => {
+      let propertyA: number | string = '';
+      let propertyB: number | string = '';
+      if (this._sort.active) {
+        const sortable: SamSortable = this._sort.sortables.get(this._sort.active);
+        const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
+        const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
+        return (valueA < valueB ? -1 : 1) * (this._sort.direction === 'asc' ? 1 : -1);
+      }
+    });
+   
   }
 }
