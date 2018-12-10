e8a3b0430e051bb599719c23ee78dbaca79d99f9
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2018-12-10 14:33:20 -0500

    removed unused code

--- src/ui-kit/experimental/hierarchical/hierarchical-tree-grid/hierarchical-tree-grid.component.ts
+++ src/ui-kit/experimental/hierarchical/hierarchical-tree-grid/hierarchical-tree-grid.component.ts
@@ -7,15 +7,11 @@ import {
   Output,
   EventEmitter
 } from '@angular/core';
-import { FormArray } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs';
 import { DataSource } from '@angular/cdk';
-
 import { SamSortDirective, SamPaginationComponent, SamSortable } from '../../../components'
-import { SamHiercarchicalServiceInterface } from '../hierarchical-interface';
 import { KeyHelper } from '../../../../ui-kit/utilities';
-// import { KeyHelper } from 'ui-kit/utilities';
 
 export interface GridTemplate {
 }
@@ -34,43 +30,6 @@ export interface GridDataSource {
 export interface GridItem {
 }
 
-// class Grid {
-//   @Input() public template: GridTemplate;
-//   @Input() public datasource: GridDataSource;
-//   @Input() public templateConfiguration: GridTemplateConfiguration;
-//   @Output() public levelChanged = new EventEmitter<GridItem>();
-//   @Output() public itemSelected = new EventEmitter<GridItem>();
-
-//   public displayedColumns = ['select'];
-//   public selected: GridItem;
-//   public focusedCell: any; // Should be the table cell
-
-//   public ngOnChanges () {
-//     this.displayedColumns = [...this.displayedColumns, ...this.templateConfiguration.displayedColumns]
-//   }
-
-//   public handleClick(): void {}
-
-//   public changeLevel(item: GridItem): void {
-//     this.levelChanged.emit(item);
-//   }
-
-//   public handleKeydown(keyEvent: any): void {
-//     switch (keyEvent.type) {
-//       case KeyHelper.is('down', keyEvent):
-//         return this.handleArrowDown(keyEvent);
-//       default: 
-//         return;
-//     }
-//   }
-
-
-
-//   private handleArrowDown (event): void {}
-
-// }
-
-/*************************************************** */
 @Component({
   selector: 'sam-hierarchical-tree-grid',
   templateUrl: './hierarchical-tree-grid.component.html',
@@ -84,7 +43,7 @@ export class SamHierarchicalTreeGridComponent implements OnInit {
   @Output() public itemSelected = new EventEmitter<GridItem>();
   @Output() public levelChanged = new EventEmitter<GridItem>();
   public selectedItem: GridItem;
-
+  public chkSelected: boolean = false;
   selectedItemIndex = 0;
 
   public displayedColumns = ['select'];
@@ -113,8 +72,10 @@ export class SamHierarchicalTreeGridComponent implements OnInit {
   }
 
   onSelectItem(ev: Event, item: GridItem) {
+
     this.selectedItem = item;
     this.itemSelected.emit(this.selectedItem);
+
   }
   handleKeyup(ev) {
     if (KeyHelper.is('tab', event)) {
@@ -183,20 +144,6 @@ export class SampleDataSource extends DataSource<any> {
   /** Returns a sorted copy of the database data. */
   sortData(data: any[]): any {
     return data;
-    //  if (!this._sort.active) { return data; }
-    //  else {
-    //   return data;
-    //  }
-    // return data.sort((a, b) => {
-    //   let propertyA: number | string = '';
-    //   let propertyB: number | string = '';
-    //   if (this._sort.active) {
-    //     const sortable: SamSortable = this._sort.sortables.get(this._sort.active);
-    //     const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
-    //     const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
-    //     return (valueA < valueB ? -1 : 1) * (this._sort.direction === 'asc' ? 1 : -1);
-    //   }
-    // });
 
   }
 }
