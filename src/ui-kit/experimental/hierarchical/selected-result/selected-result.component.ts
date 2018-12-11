aee482bde84a1930e9aa45c79dbad7759d7490f2
Thomas Tighe <ttighe@reisystems.com>
2018-12-11 09:33:11 -0500

    adding properties and basic ul li

--- src/ui-kit/experimental/hierarchical/selected-result/selected-result.component.ts
+++ src/ui-kit/experimental/hierarchical/selected-result/selected-result.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, OnInit, Input, ViewChild, TemplateRef,
+  ElementRef
+} from '@angular/core';
+import { SamHiercarchicalServiceInterface } from '../hierarchical-interface';
+import { KeyHelper, KEYS } from '../../../utilities/key-helper/key-helper';
+import { HierarchicalTreeSelectedItemModel } from '../hierarchical-tree-selectedItem.model';
 
 @Component({
   selector: 'sam-selected-result',
@@ -9,7 +15,52 @@ export class SamHierarchicalSelectedResultComponent implements OnInit {
 
   constructor() { }
 
+
+  /**
+  * 
+  */
+  @ViewChild('resultsList') resultsListElement: ElementRef;
+
+  /**
+ * Allow to insert a customized template for suggestions to use
+ */
+  @Input() itemTemplate: TemplateRef<any>;
+
+  /**
+   * The data model that has the selected item
+   */
+  @Input()
+  public model: HierarchicalTreeSelectedItemModel;
+
+  @Input()
+  public settings: SelectedResultSettings;
+
   ngOnInit() {
+
   }
 
+  removeItem(item: object) {
+    this.model.removeItem(item, this.settings.keyField);
+  }
+
+
+
+}
+
+export class SelectedResultSettings {
+
+  /**
+   *  This is the primary field used to identify each object in the results
+   */
+  public keyField: string;
+
+  /**
+   *  Property from supplied model used for the top part of the basic template
+   */
+  public valueProperty: string;
+
+  /**
+   *  Property from supplied model used for the bottom part of the basic template
+   */
+  public subValueProperty: string;
 }
