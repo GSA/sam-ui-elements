4a9fbf480c76e72ea77bb6044f7d3554957d893e
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2018-12-12 10:41:12 -0500

    Sy grid (#185)
    * hierarchy changes
    
    * datasource changes
    
    * data table working
    
    * sam data table working
    
    * data table working
    
    * sam data table working
    
    * check box added to the grid
    
    * order changes and checkbox changes
    
    * data service
    
    * data table changes
    
    * data table works without example data source
    
    * data source table working
    
    * removed unused code
    
    * Prototyping
    
    * database is working
    
    * check box changes
    
    * unsed code removed
    
    * index removed
    
    * removed unused code

--- src/ui-kit/experimental/hierarchical/hierarchical-tree-grid/hierarchical-tree-grid.component.ts
+++ src/ui-kit/experimental/hierarchical/hierarchical-tree-grid/hierarchical-tree-grid.component.ts
@@ -1,4 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs';
+import { DataSource } from '@angular/cdk';
+import { SamSortDirective, SamPaginationComponent, SamSortable } from '../../../components'
+import { KeyHelper } from '../../../../ui-kit/utilities';
+
+export interface GridTemplate {
+}
+
+export interface GridTemplateConfiguration {
+  displayedColumns: any[];
+  type: string
+}
+
+export interface GridDataSource {
+  connect: () => void;
+  disconnect: () => void;
+  sortData?: () => void;
+}
+
+export interface GridItem {
+}
 
 @Component({
   selector: 'sam-hierarchical-tree-grid',
@@ -6,10 +36,116 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hierarchical-tree-grid.component.scss']
 })
 export class SamHierarchicalTreeGridComponent implements OnInit {
+  @Input() public templateConfigurations: GridTemplateConfiguration;
+  @Input() public template: GridTemplate;
+  @Input() public dataSource: any;
 
-  constructor() { }
+  @Output() public itemSelected = new EventEmitter<GridItem>();
+  @Output() public levelChanged = new EventEmitter<GridItem>();
+  public selectedItem: GridItem;
+  public chkSelected: boolean = false;
+  selectedItemIndex = 0;
 
-  ngOnInit() {
+  public displayedColumns = ['select'];
+  public samTableDataSource: any | null;
+  dataChange: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
+  public focusedCell: any;
+  public focusedtemIndex = 0;
+
+
+  @ViewChild(SamPaginationComponent) paginator: SamPaginationComponent;
+  @ViewChild(SamSortDirective) sort: SamSortDirective;
+  @ViewChild('filter') filter: ElementRef;
+
+
+  ngOnChanges() {
+    this.displayedColumns = [...this.displayedColumns, ...this.templateConfigurations.displayedColumns];
   }
 
+  ngOnInit() {
+    this.dataChange.next(this.dataSource);
+    this.samTableDataSource = new SampleDataSource(
+      this.dataChange,
+      this.paginator,
+      this.sort
+    );
+  }
+
+  onSelectItem(ev: Event, item: GridItem) {
+
+    this.selectedItem = item;
+    this.itemSelected.emit(this.selectedItem);
+
+  }
+  handleKeyup(ev) {
+    if (KeyHelper.is('tab', event)) {
+      return
+    }
+    if (KeyHelper.is('down', event)) {
+      console.log('onDownArrowDown')
+    }
+
+    // On up arrow press
+    if (KeyHelper.is('up', event)) {
+      console.log('onUpArrowDown')
+    }
+
+    console.log(ev)
+  }
+
+  isSelected(item: any) {
+    return this.selectedItem ?
+      this.focusedCell.id == item.id : false;
+  }
+  public onChangeLevel(ev: Event, item: GridItem): void {
+    this.levelChanged.emit(item);
+
+  }
 }
+
+export class SampleDataSource extends DataSource<any> {
+  totalcost = 0;
+  _filterChange = new BehaviorSubject('');
+  get filter(): string { return this._filterChange.value; }
+  set filter(filter: string) { this._filterChange.next(filter); }
+  filteredData: any[] = [];
+  renderedData: any[] = [];
+
+  constructor(private dataChange: any,
+    private _paginator: SamPaginationComponent,
+    private _sort: SamSortDirective) {
+    super();
+  }
+
+  connect(): Observable<any[]> {
+    const displayDataChanges = [
+      this.dataChange,
+      //this._sort.samSortChange,
+      this._filterChange,
+    ];
+    return Observable.merge(...displayDataChanges).map(() => {
+
+      const filteredData = this.dataChange.value.slice().filter((item: any) => {
+        const searchStr = (item.id + item.name).toLowerCase();
+        return searchStr.indexOf(this.filter.toLowerCase()) !== -1;
+      });
+      // set total
+      this.totalcost = 0;
+      filteredData.map((item: any) => {
+        this.totalcost += item.cost;
+      });
+      // Sort filtered data
+      const sortedData = this.sortData(filteredData.slice());
+      this.renderedData = sortedData;
+      return this.renderedData;
+    });
+  }
+  disconnect() { }
+  /** Returns a sorted copy of the database data. */
+  sortData(data: any[]): any {
+    return data;
+
+  }
+}
+
+
