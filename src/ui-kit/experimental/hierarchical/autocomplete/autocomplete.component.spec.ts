b21e376cc636022a7e60ea8343d307fe0118827f
Thomas Tighe <ttighe@reisystems.com>
2018-12-18 10:27:29 -0500

    fix test

--- src/ui-kit/experimental/hierarchical/autocomplete/autocomplete.component.spec.ts
+++ src/ui-kit/experimental/hierarchical/autocomplete/autocomplete.component.spec.ts
@@ -9,7 +9,7 @@ import { By } from '@angular/platform-browser';
 import 'rxjs/add/observable/of';
 
 
-fdescribe('SamHierarchicalAutocompleteComponent', () => {
+describe('SamHierarchicalAutocompleteComponent', () => {
   let component: SamHierarchicalAutocompleteComponent;
   let fixture: ComponentFixture<SamHierarchicalAutocompleteComponent>;
 
