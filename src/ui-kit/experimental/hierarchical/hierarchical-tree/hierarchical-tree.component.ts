073552c67dca456377ab5807c7f4823eec20ad62
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2018-12-27 15:27:15 -0500

    code changes related to hierarchy tree

--- src/ui-kit/experimental/hierarchical/hierarchical-tree/hierarchical-tree.component.ts
+++ src/ui-kit/experimental/hierarchical/hierarchical-tree/hierarchical-tree.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
+import { OptionsType } from '../../../../ui-kit/types';
+import { Observable, BehaviorSubject } from 'rxjs';
+
+
+export interface HierarchyConfiguration {
+  displayedColumns: any[],
+  primaryKey: string,
+  options: OptionsType[],
+  gridData: any[],
+  filterText: string
+}
 
 @Component({
   selector: 'sam-hierarchical-tree',
   templateUrl: './hierarchical-tree.component.html',
   styleUrls: ['./hierarchical-tree.component.scss']
 })
+
 export class SamHierarchicalTreeComponent implements OnInit {
 
-  constructor() { }
+  public selectedAgency$ = new BehaviorSubject<any>(null);
+  public selectResults$ = new BehaviorSubject<any[]>([]);
+  public filterText$ = new BehaviorSubject<any>('');
+  /**
+  * hierarchy tree picker configurations 
+  */
+  @Input() hierarchyConfiguration: HierarchyConfiguration;
 
-  ngOnInit() {
+  /**
+  * Data for the Table.
+  *  Observable data array
+  * Stream that emit a array each time when the item is selected.
+  * Stream that changes each time when click action trigger on row.
+  */
+  @Input() gridData: Observable<any>;
+
+  /**
+  * Event emitted when row is clicked
+  */
+  @Output() public rowChanged = new EventEmitter<any>();
+
+  /**
+  * Event emitted when level change is clicked
+  */
+  @Output() public selectedAgency = new EventEmitter<string>();
+
+  /**
+  * Event emitted when row set is selected.
+  */
+  @Output() selectResults = new EventEmitter<any[]>();
+
+
+  public ngOnInit() {
+    this.selectedAgency$.subscribe(
+      value => this.selectedAgency.emit(value)
+    );
+    this.selectResults$.subscribe(
+      res => this.selectResults.emit(res)
+    );
+    this.filterText$.subscribe(
+      text => this.hierarchyConfiguration.filterText = text);
   }
 
 }
