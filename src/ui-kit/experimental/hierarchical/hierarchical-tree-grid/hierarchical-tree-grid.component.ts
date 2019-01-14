b45e49564eca66cc3ad472d54cbfcf39e76c3470
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2019-01-14 09:52:39 -0500

    header unit test cases (#208)
    * added test cases
    
    * data source test cases
    
    * data source separated into a file
    
    * clean up code

--- src/ui-kit/experimental/hierarchical/hierarchical-tree-grid/hierarchical-tree-grid.component.ts
+++ src/ui-kit/experimental/hierarchical/hierarchical-tree-grid/hierarchical-tree-grid.component.ts
@@ -2,16 +2,14 @@ import {
   Component,
   OnInit,
   ViewChild,
-  ElementRef,
   Input,
   Output,
   EventEmitter,
   ChangeDetectorRef
 } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { Observable } from 'rxjs';
-import { DataSource } from '@angular/cdk';
-import { SamSortDirective, SamPaginationComponent, SamSortable } from '../../../components'
+import { SamSortDirective } from '../../../components'
+import { HierarchicalDataSource } from './data-source';
 
 export interface GridTemplate {
 }
@@ -75,7 +73,6 @@ export class SamHierarchicalTreeGridComponent implements OnInit {
   public displayedColumns = ['select'];
   public columnFieldName = [];
   public columnHeaderText = [];
-
   public selectedList: object[] = [];
   public hierarchicalDataSource: HierarchicalDataSource | null;
   public dataChange: BehaviorSubject<object[]> = new BehaviorSubject<object[]>([]);
@@ -137,55 +134,4 @@ export class SamHierarchicalTreeGridComponent implements OnInit {
       this.rowChanged.emit(row[this.templateConfigurations.primaryKey]);
     }
   }
-}
-
-// preparing data source for the hierarchical grid
-export class HierarchicalDataSource extends DataSource<any> {
-  _filterChange = new BehaviorSubject('');
-  get filter(): string { return this._filterChange.value; }
-  set filter(filter: string) { this._filterChange.next(filter); }
-  filteredData: any[] = [];
-  renderedData: any[] = [];
-
-  constructor(private dataChange: any,
-    private _sort: SamSortDirective) {
-    super();
-  }
-
-  connect(): Observable<any[]> {
-    const displayDataChanges = [
-      this.dataChange,
-      this._sort.samSortChange,
-      this._filterChange,
-    ];
-    return Observable.merge(...displayDataChanges).map(() => {
-      const filteredData = this.dataChange.value.slice().filter((item: any) => {
-        const searchStr = JSON.stringify(item).toLowerCase();
-        return searchStr.indexOf(this.filter.toLowerCase()) !== -1;
-      });
-
-      // Sort filtered data
-      const sortedData = this.getSortedData(filteredData.slice());
-      this.renderedData = sortedData;
-      return this.renderedData;
-    });
-  }
-  disconnect() { }
-  /** Returns a sorted copy of the database data. */
-
-  getSortedData(data: any[]): any[] {
-    if (!this._sort.active || this._sort.direction === '') { return data; }
-    return data.sort((a, b) => {
-      let propertyA = this.sortingDataAccessor(a, this._sort.active);
-      let propertyB = this.sortingDataAccessor(b, this._sort.active)
-      const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
-      const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
-      return (valueA < valueB ? -1 : 1) * (this._sort.direction === 'asc' ? 1 : -1);
-    });
-  }
-  sortingDataAccessor: ((data: any, sortHeaderId: string) => string | number) =
-    (data: any, sortHeaderId: string): string | number => {
-      const value = (data as { [key: string]: any })[sortHeaderId];
-      return value;
-    }
 }
