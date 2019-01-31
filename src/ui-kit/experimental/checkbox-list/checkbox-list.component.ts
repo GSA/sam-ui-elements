dd2a2cf9956c8c0066db58bcc86624014a4b1576
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2019-01-31 15:45:17 -0500

    keyboard interactions added

--- src/ui-kit/experimental/checkbox-list/checkbox-list.component.ts
+++ src/ui-kit/experimental/checkbox-list/checkbox-list.component.ts
@@ -1,7 +1,7 @@
 import {
   Component, Output,
   forwardRef, Input,
-  EventEmitter,ElementRef, ViewChild
+  EventEmitter, ElementRef, ViewChild
 } from '@angular/core';
 import {
   FormControl,
@@ -29,25 +29,25 @@ export interface OptionModel {
 })
 
 
-export class SamCheckboxListComponent  {
-  @Input() headerText: string;
+export class SamCheckboxListComponent {
+  @Input() accordionText: string;
 
   @Input() public isSingleMode: boolean;
 
-  @Input() options: OptionModel[] = [
-    { name: 'id1', value: 'test', label: 'test-id1', required: false, checked: false },
-    { name: 'id2', value: 'test', label: 'test-id2', required: true,  checked: true },
-    { name: 'id3', value: 'test', label: 'test-id3', required: false, checked: false },
-    { name: 'id4', value: 'test', label: 'test-id4', required: false, checked: false },
-    { name: 'id5', value: 'test', label: 'test-id5', required: false, checked: true },
-    { name: 'id6', value: 'test', label: 'test-id6', required: false, checked: false },
-    { name: 'id7', value: 'test', label: 'test-id7', required: false, checked: false },
-    { name: 'id8', value: 'test', label: 'test-id8', required: false, checked: false },
-  ];
+  @Input() options: OptionModel[];
+  /**
+  * List of the items selected by the checkboxes or the radio buttons
+  */
+  public selectedList: OptionModel[] = [];
 
-   /**
-   * Ul list of elements 
-   */
+  /**
+ * Event emitted when row set is selected.
+ */
+  @Output() selectResults = new EventEmitter<OptionModel[]>();
+
+  /**
+  * Ul list of elements 
+  */
   @ViewChild('checkboxList') checkboxListElement: ElementRef;
 
   private highlightedIndex: number = 0;
@@ -60,30 +60,61 @@ export class SamCheckboxListComponent  {
 
   optionsMode: string = 'checkbox';
   ngOnInit() {
+    this.options.forEach(item=>{
+      if(item.checked) {
+        this.emitSelectedList(item);
+      } 
+    })
     this.optionsMode = this.isSingleMode ? 'radio' : 'checkbox';
   }
 
- onKeyup(event) {
-  if (KeyHelper.is(KEYS.TAB, event)) {
-    return;
+  emitSelectedList(option: OptionModel): void {
+    this.selectedList = [...this.selectedList, option];
+    this.selectResults.emit(this.selectedList);
   }
-  else if (KeyHelper.is(KEYS.DOWN, event)) {
-    this.onArrowDown();
+
+  onKeyup(event) {
+    if (KeyHelper.is(KEYS.TAB, event)) {
+      return;
     }
-  else if (KeyHelper.is(KEYS.UP, event)) {
-    this.onArrowUp();
+    else if (KeyHelper.is(KEYS.DOWN, event)) {
+      this.onArrowDown();
+    }
+    else if (KeyHelper.is(KEYS.UP, event)) {
+      this.onArrowUp();
+    }
+    else if (KeyHelper.is(KEYS.SPACE, event)) {
+      console.log('space clicked')
+      this.onChecked(event,this.highlightedItem);
+    }
+    
+    event.stopPropagation();
   }
-  else if (KeyHelper.is(KEYS.SPACE, event)) {
-   this.selectItem(this.highlightedItem);
+
+  /**
+    * On select the results
+    */
+  onChecked(ev, option: OptionModel): void {
+    if (ev.target.checked) {
+      if (this.isSingleMode) {
+        this.selectedList = [option];
+      } else {
+        this.selectedList = [...this.selectedList, option];
+      }
+    } else {
+      const index: number = this.selectedList.indexOf(option);
+      if (index !== -1) {
+        this.selectedList = this.selectedList.filter(item => item !== option);
+      }
+    }
+    this.selectResults.emit(this.selectedList);
   }
-  event.stopPropagation();
- }
 
   private onArrowUp(): void {
     if (this.options && this.options.length > 0) {
       if (this.highlightedIndex !== 0) {
         this.highlightedIndex--;
-        console.log(this.highlightedIndex);
+       
         this.setHighlightedItem(this.options[this.highlightedIndex]);
         this.scrollSelectedItemToTop();
       }
@@ -94,7 +125,6 @@ export class SamCheckboxListComponent  {
     if (this.options && this.options.length > 0) {
       if (this.highlightedIndex < this.options.length - 1) {
         this.highlightedIndex++;
-        console.log(this.highlightedIndex);
         this.setHighlightedItem(this.options[this.highlightedIndex]);
         this.scrollSelectedItemToTop();
       }
@@ -116,18 +146,11 @@ export class SamCheckboxListComponent  {
     let selectedChild = this.checkboxListElement.nativeElement.children[this.highlightedIndex];
     this.checkboxListElement.nativeElement.scrollTop = selectedChild.offsetTop;
   }
-  
- 
+
+
   listItemHover(index: number): void {
     this.highlightedIndex = index;
     this.setHighlightedItem(this.options[this.highlightedIndex]);
   }
-
-  public selectItem(item: OptionModel): void {
-  item.checked = !item.checked;
-  console.log(item,'selected results');
-  }
-
-  
 
 }
