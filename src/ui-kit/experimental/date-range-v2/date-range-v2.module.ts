7c1f31e447900b3dc36e90916071900d3ac945e6
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2019-02-12 09:13:41 -0500

    Datepicker changes

--- src/ui-kit/experimental/date-range-v2/date-range-v2.module.ts
+++ src/ui-kit/experimental/date-range-v2/date-range-v2.module.ts
@@ -3,12 +3,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { SamDateRangeV2Component } from './date-range-v2.component';
 
+//import {A11yModule} from '@angular/cdk';
+import {OverlayModule} from '../../experimental/patterns/layout/components/core/overlay';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
     imports: [CommonModule,
-        FormsModule,
-        ReactiveFormsModule
+      // MatButtonModule,
+       MatDialogModule,
+       OverlayModule,
+      // A11yModule,
     ],
     declarations: [SamDateRangeV2Component],
     exports: [SamDateRangeV2Component]
