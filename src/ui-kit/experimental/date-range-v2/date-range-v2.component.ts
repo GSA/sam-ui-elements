de11f02eaa5522a91accae2d41e6fca518ee4277
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2019-02-15 11:13:53 -0500

    start date and end date added

--- src/ui-kit/experimental/date-range-v2/date-range-v2.component.ts
+++ src/ui-kit/experimental/date-range-v2/date-range-v2.component.ts
@@ -1,21 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 
+export interface DateConfig {
+  name: string,
+  placeholder: string,
+  label: string,
+  hint: string,
+  date: any
+}
+export interface SelectedModel {
+  startDate: any,
+  endDate: any
+}
 @Component({
   selector: 'sam-date-range-v2',
   templateUrl: './date-range-v2.component.html',
   styleUrls: ['./date-range-v2.component.scss']
 })
-export class SamDateRangeV2Component implements OnInit {
-  example1;
-  example2 = '12/12/2017';
-  example3;
-  example4;
-  picker;
-  resultPicker2;
-  date;
-  constructor() { }
+export class SamDateRangeV2Component {
 
-  ngOnInit() {
+  public selectedModel: any = {};
+
+  @Input() startDateConfig: DateConfig;
+
+  @Input() endDateConfig: DateConfig;
+
+  /**
+  * Event emitted when row set is selected.
+  */
+  @Output() selectedDates = new EventEmitter<SelectedModel>();
+
+  startDateChange(newStartDate) {
+    this.selectedModel.startDate = newStartDate;
+    this.selectedDates.emit(this.selectedModel);
   }
 
+  endDateChange(newEndDate) {
+    this.selectedModel.endDate = newEndDate;
+    this.selectedDates.emit(this.selectedModel);
+  }
 }
