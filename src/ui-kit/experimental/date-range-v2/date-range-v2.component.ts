e80b9a70fe2965d78356194fb735406c3c66586f
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2019-02-15 21:01:03 -0500

    ControlValueAccessor changes

--- src/ui-kit/experimental/date-range-v2/date-range-v2.component.ts
+++ src/ui-kit/experimental/date-range-v2/date-range-v2.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import {
+  Component,
+  OnInit, Input, Output, EventEmitter, forwardRef
+} from '@angular/core';
+
+import {
+ ControlValueAccessor,
+ NG_VALUE_ACCESSOR,
+ FormControl,
+} from '@angular/forms';
 
 export interface DateConfig {
   name: string,
@@ -14,37 +23,85 @@ export interface SelectedModel {
 @Component({
   selector: 'sam-date-range-v2',
   templateUrl: './date-range-v2.component.html',
-  styleUrls: ['./date-range-v2.component.scss']
+  styleUrls: ['./date-range-v2.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => SamDateRangeV2Component),
+    multi: true
+  }]
 })
-export class SamDateRangeV2Component implements OnInit{
+export class SamDateRangeV2Component implements OnInit, ControlValueAccessor {
+
+  /**
+* Deprecated, Sets the bound value of the component
+*/
+@Input() model: any = {};
 
   public selectedModel: any = {};
 
   @Input() startDateConfig: DateConfig;
 
   @Input() endDateConfig: DateConfig;
-
-  ngOnInit(){
-    this.selectedModel.startDate = this.startDateConfig.date;
-    this.selectedModel.endDate = this.endDateConfig.date;
-    this.emit()
-  }
-
-  emit(){
-    this.selectedDates.emit(this.selectedModel);
-  }
-  /**
-  * Event emitted when row set is selected.
+ /**
+  * Sets the angular FormControl
   */
-  @Output() selectedDates = new EventEmitter<SelectedModel>();
+ @Input() control: FormControl;
+ 
+  /**
+  * Deprecated, Event emitted when the model value changes
+  */
+ @Output() modelChange: EventEmitter<any> = new EventEmitter<any>();
+
+  get value() {
+    return this.model;
+  }
+  set value(val) {
+    this.setSelectedDate(val);
+    this.onChange(this.model);
+    this.onTouched();
+  }
+  onChange: any = (c) => undefined;
+  onTouched: any = () => undefined;
+
+  ngOnInit() {
+    this.selectedModel.startDate = this.model.startDate;
+    this.selectedModel.endDate = this.model.endDate;
+  }
+
+  emit() {
+    this.selectedModel = this.model;
+    this.modelChange.emit(this.model);
+  }
 
   startDateChange(newStartDate) {
-    this.selectedModel.startDate = newStartDate;
-    this.emit()
+    this.model.startDate = newStartDate;
+   this.emit()
   }
 
   endDateChange(newEndDate) {
-    this.selectedModel.endDate = newEndDate;
+    this.model.endDate = newEndDate;
     this.emit()
+  }
+
+  setSelectedDate(val: any) {
+    this.model.startDate = val.startDate;
+    this.model.endDate = val.endDate;
+    this.selectedModel = this.model;
+   this.emit()
+  }
+  registerOnChange(fn) {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn) {
+    this.onTouched = fn;
+  }
+
+  writeValue(value) {
+    let returnValue = value;
+    if (!returnValue) {
+      returnValue = {};
+    }
+    
+    this.setSelectedDate(returnValue);
   }
 }
