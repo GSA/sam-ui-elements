04f681c46c7c2a029274f5eb5488658c667043e5
Thomas Tighe <ttighe@reisystems.com>
2019-02-25 12:58:00 -0500

    removing some white space and an unused import

--- src/ui-kit/experimental/hierarchical/hierarchical-tree/hierarchical-tree.component.spec.ts
+++ src/ui-kit/experimental/hierarchical/hierarchical-tree/hierarchical-tree.component.spec.ts
@@ -11,7 +11,6 @@ import { FormsModule } from '@angular/forms';
 import { CdkTableModule } from '@angular/cdk';
 import { SamDataTableModule, SamSortDirective } from '../../../components/data-table';
 import { SamHierarchicalTreeConfiguration } from '../models/SamHierarchicalTreeConfiguration';
-import { ExpectedConditions } from 'protractor';
 import { Observable } from 'rxjs';
 import { SamHiercarchicalServiceInterface,SamHiercarchicalServiceSearchItem, SamHiercarchicalServiceResult } from '../hierarchical-interface';
 import 'rxjs/add/observable/of';
