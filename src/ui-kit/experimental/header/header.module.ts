212a039ecebc8bab81722205e043e4e57ffbce44
Thomas Tighe <ttighe@reisystems.com>
2019-04-04 13:25:54 -0400

    adding angualr routing setup

--- src/ui-kit/experimental/header/header.module.ts
+++ src/ui-kit/experimental/header/header.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SamHeaderComponent } from './header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule, FormsModule
+    CommonModule, FormsModule, RouterModule
   ],
   declarations: [SamHeaderComponent],
   exports: [SamHeaderComponent]
