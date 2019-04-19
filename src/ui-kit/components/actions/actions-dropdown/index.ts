a10356dac9bd00ae59f2a0845885a8851f4765eb
anuragmundada-reisys <anurag.mundada@reisystems.com>
2019-04-19 11:30:24 -0400

    [Ready for Review]: IAE-16876 Fix 508 Accessibility and JAWS issues with actions-dropwdown component (#336)
    * IAE-16876 Make Action Dropdown button and Menu 508 Accessible
    
    * fix tests
    
    * fix unit tests
    
    * small changes as requested

--- src/ui-kit/components/actions/actions-dropdown/index.ts
+++ src/ui-kit/components/actions/actions-dropdown/index.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SamActionsDropdownComponent } from './actions-dropdown.component';
+import {SamDirectivesModule} from "../../../directives";
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, SamDirectivesModule],
     declarations: [ SamActionsDropdownComponent ],
     exports: [ SamActionsDropdownComponent ],
 })
