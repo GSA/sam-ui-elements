965bd9eebe8314d1ce18360adc8e0e9bdf0c6ffa
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2019-05-03 10:26:25 -0400

    Verbaige changes

--- src/ui-kit/form-templates/international-phone/sam-extension/extension.component.ts
+++ src/ui-kit/form-templates/international-phone/sam-extension/extension.component.ts
@@ -107,7 +107,7 @@ export class SamExtension extends SamFormControl {
   private extensionValidator (c: FormControl) {
     const regex: RegExp = /^[0-9]{1,20}$/g;
     const message =
-      'Phone extensions must be less than 20 numbers';
+      'Phone extensions must be 20 digits or fewer';
 
     return c && c.value && !c.value.toString().match(regex)
       ? { extension: { message: message } }
