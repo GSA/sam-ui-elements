72d478196ebb2d576e01e45a000b53861ef734f1
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2019-08-02 13:54:26 -0400

    aria expanded added

--- src/ui-kit/components/sidenav/menu-item/menu-item.component.ts
+++ src/ui-kit/components/sidenav/menu-item/menu-item.component.ts
@@ -24,6 +24,11 @@ export class SamMenuItemComponent {
    */
   @Output() selection: EventEmitter<any> = new EventEmitter<any>();
 
+    /**
+   * Section is visible 
+   */
+  public showSection: boolean = false;
+
   constructor(private service: SidenavService) { }
 
   updateUI(index: number, event: Event, menuItem: MenuItem): void {
@@ -44,5 +49,12 @@ export class SamMenuItemComponent {
     this.data.emit(event);
     this.selection.emit(event);
     return;
+  }
+
+   /**
+   * Closes the Section
+   */
+  close() {
+    this.showSection = false;
   }
 }
