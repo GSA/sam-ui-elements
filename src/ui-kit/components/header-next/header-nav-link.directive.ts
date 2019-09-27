fed9725a5ba9a61de64459aec50d71fa08042fbc
Carlos Valle <carlos.m.valle@gmail.com>
2019-09-27 16:34:15 -0400

    Add header ui updates for tablet breakpoint

--- src/ui-kit/components/header-next/header-nav-link.directive.ts
+++ src/ui-kit/components/header-next/header-nav-link.directive.ts
@@ -2,7 +2,6 @@ import {Input, Directive, HostBinding } from '@angular/core';
 
 @Directive({ selector: '[sam-header-nav-link]' })
 export class SamHeaderNavLink {
-  @HostBinding('class.usa-nav__link') public linkClass: boolean = true;
   @HostBinding('class.usa-current') @Input() active: boolean = false;
   constructor() {}
 }
