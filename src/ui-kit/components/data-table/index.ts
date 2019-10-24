6152efe70d3432ada16e37d20a5ab3d3c37cd157
Yerram_Shilpa <yerram_shilpa@Shilpas-MacBook-Pro.local>
2019-10-23 20:04:29 -0400

    cdk reference changes

--- src/ui-kit/components/data-table/index.ts
+++ src/ui-kit/components/data-table/index.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {CdkTableModule} from '@angular/cdk';
+import {CdkTableModule} from '@angular/cdk/table';
 import {SamDataTableComponent} from './data-table.component';
 import {SamCellDirective, SamHeaderCellDirective} from './cell.component';
 import {SamHeaderRowComponent, SamRowComponent} from './row.component';
