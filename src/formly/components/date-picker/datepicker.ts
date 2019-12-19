64ab9b061a0cb2ffbe54e3485a346508b49de0cd
Thomas Tighe <ttighe@reisystems.com>
2019-12-19 08:13:24 -0500

    clean up

--- src/formly/components/date-picker/datepicker.ts
+++ src/formly/components/date-picker/datepicker.ts
@@ -7,16 +7,7 @@ import { FieldType } from '@ngx-formly/core';
   <input [id]="id" [class.usa-input--error]="showError" [formControl]="formControl" [formlyAttributes]="field" matInput [min]="to.minDate" [max]="to.maxDate"  [matDatepicker]="picker" placeholder="Choose a date">
 <mat-datepicker-toggle matSuffix [for]="picker"></mat-datepicker-toggle>
 <mat-datepicker  [startAt]="to.startDate"   #picker></mat-datepicker>
-<style>
-.mat-datepicker-toggle > .mat-button-base:hover{
-  background-color: ffffff !important;
- }
- 
- .mat-input-element
- { display:inline-block; 
-max-width: 200px;
-}
-</style> 
+
   `,
 })
 export class FormlyFieldDatePickerComponent extends FieldType { }
