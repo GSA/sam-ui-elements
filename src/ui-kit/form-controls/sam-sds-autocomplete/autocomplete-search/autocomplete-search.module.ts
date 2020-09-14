63b400f5310ada55d71a20c6b7a247f52b6aaee3
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2020-09-14 15:56:52 -0400

    IAE-40295 Autocomplete Up to date with SDS and added delimeter and Focus handle changes (#516)
    * updated changes for auto complete
    
    * updated compile error
    
    * updated icons
    
    * Update styles to align with SDS
    
    * Updated test coverage
    
    * test updated
    
    * delemeter changes
    
    * Delimeter changes with enter
    
    * updated demo
    
    * updated demo
    
    * updated close icon changes
    
    * updated focus removed changes
    
    * focus changes
    
    * updated focusout model clear
    
    * focus out changes
    
    * changes
    
    Co-authored-by: davereed <dave@contagious.ly>

--- src/ui-kit/form-controls/sam-sds-autocomplete/autocomplete-search/autocomplete-search.module.ts
+++ src/ui-kit/form-controls/sam-sds-autocomplete/autocomplete-search/autocomplete-search.module.ts
@@ -1,17 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { SAMSDSAutocompleteSearchComponent } from './autocomplete-search.component';
-import { SamDirectivesModule } from '../../../directives';
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { SAMSDSAutocompleteSearchComponent } from "./autocomplete-search.component";
+import { SamDirectivesModule } from "../../../directives";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    SamDirectivesModule
-  ],
+  imports: [CommonModule, FormsModule, SamDirectivesModule, FontAwesomeModule],
   declarations: [SAMSDSAutocompleteSearchComponent],
-  exports: [SAMSDSAutocompleteSearchComponent]
+  exports: [SAMSDSAutocompleteSearchComponent],
 })
 export class SAMSDSAutocompleteSearchModule {}
--- src/ui-kit/form-controls/sam-sds-autocomplete/autocomplete/models/SDSAutocompletelConfiguration.model.ts
+++ src/ui-kit/form-controls/sam-sds-autocomplete/autocomplete/models/SDSAutocompletelConfiguration.model.ts
@@ -1,11 +1,14 @@
-import { SDSSelectedResultConfiguration } from '../../selected-result/models/SDSSelectedResultConfiguration';
-import { SAMSDSAutocompleteSearchConfiguration } from '../../autocomplete-search/models/SAMSDSAutocompleteConfiguration';
-import { SelectionMode } from '../../selected-result/models/sds-selected-item-model-helper';
+import { SDSSelectedResultConfiguration } from "../../selected-result/models/SDSSelectedResultConfiguration";
+import { SAMSDSAutocompleteSearchConfiguration } from "../../autocomplete-search/models/SAMSDSAutocompleteConfiguration";
+import { SelectionMode } from "../../selected-result/models/sds-selected-item-model-helper";
 
-export class SAMSDSAutocompletelConfiguration implements SDSSelectedResultConfiguration, SAMSDSAutocompleteSearchConfiguration {
+export class SAMSDSAutocompletelConfiguration
+  implements
+    SDSSelectedResultConfiguration,
+    SAMSDSAutocompleteSearchConfiguration {
   /**
-     * sets the default debounce time to 250 milliseconds 
-     */
+   * sets the default debounce time to 250 milliseconds
+   */
   constructor() {
     this.debounceTime = 250;
     this.minimumCharacterCountSearch = 0;
@@ -56,7 +59,6 @@ export class SAMSDSAutocompletelConfiguration implements SDSSelectedResultConfig
    */
   public selectionMode: SelectionMode = SelectionMode.SINGLE;
 
-
   /**
    * Allows option to allow user text not in the standard results
    */
@@ -65,15 +67,50 @@ export class SAMSDSAutocompletelConfiguration implements SDSSelectedResultConfig
   /**
    * Text appeneded ad the end of free text
    */
-  public freeTextSubtext: string = 'search';
+  public freeTextSubtext: string = "search";
 
   /**
    * Focus into autocomplete search
    */
   public focusInSearch: boolean = true;
 
-    /**
+  /**
    * The aria-label for the auto-complete
    */
-  public ariaLabelText: string = 'Auto Complete';
+  public ariaLabelText: string = "Auto Complete";
+
+  /**
+   * To enable the tag mode
+   */
+  public isTagModeEnabled: boolean = false;
+
+  /**
+   * To make input readonly
+   */
+  public inputReadOnly = false;
+
+  /**
+   * Name of the children filed
+   */
+  public groupByChild: string = "children";
+
+  /**
+   * To enable the Grouping mode
+   */
+  public isGroupingEnabled: boolean = false;
+
+  /**
+   * To enable the Group item selectable
+   */
+  public isSelectableGroup: boolean = true;
+
+  /**
+   * To enable the delimeter
+   */
+  public isDelimiterEnabled: boolean = true;
+
+  /**
+   *  The delimeter array
+   */
+  public delimiters: string | string[] = [",", ";"];
 }
