4433e968e82703b5cdaa900e119bd3d3e5b57ec3
Frank Pigeon Jr. <frank.pigeon@gsa.gov>
2026-08-18 15:17:20 -0500

    chore: migrate TSLint to ESLint with a11y gate (#579)

---
+++ test-app/eslint.config.mjs
@@ -0,0 +1,41 @@
+import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
+
+const asWarnings = (configs) => Object.fromEntries(
+  configs.flatMap((config) => Object.entries(config.rules || {}))
+    .map(([rule, setting]) => [
+      rule,
+      setting === 'off' || setting === 0
+        ? 'off'
+        : Array.isArray(setting)
+          ? ['warn', ...setting.slice(1)]
+          : 'warn',
+    ]),
+);
+
+const recommendedTypeScriptWarnings = asWarnings([
+  ...tseslint.configs.recommended,
+  ...angular.configs.tsRecommended,
+]);
+
+const accessibilityWarnings = asWarnings(
+  angular.configs.templateAccessibility,
+);
+
+export default tseslint.config(
+  { ignores: ['coverage/**', 'dist/**', 'node_modules/**', 'src/components/**'] },
+  {
+    files: ['**/*.ts'],
+    extends: [
+      ...tseslint.configs.recommended,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: recommendedTypeScriptWarnings,
+  },
+  {
+    files: ['**/*.html'],
+    extends: angular.configs.templateAccessibility,
+    rules: accessibilityWarnings,
+  },
+);
