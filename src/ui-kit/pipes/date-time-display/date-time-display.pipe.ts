e91271b0bd032088a28154ce3648bf022e6d2065
Frank Pigeon Jr. <4629398+fpigeonjr@users.noreply.github.com>
2026-09-03 14:39:13 -0500

    Fix Prettier formatting on date-time-display.pipe.ts

--- src/ui-kit/pipes/date-time-display/date-time-display.pipe.ts
+++ src/ui-kit/pipes/date-time-display/date-time-display.pipe.ts
@@ -7,9 +7,7 @@ import moment from "moment";
   standalone: false,
 })
 export class DateTimeDisplayPipe implements PipeTransform {
-  transform(
-    datetime: string | number | Date | undefined
-  ): string | undefined {
+  transform(datetime: string | number | Date | undefined): string | undefined {
     if (datetime === undefined) {
       console.warn("Invalid value passed into DateTimeDisplayPipe");
       return undefined;
